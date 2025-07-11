ba4f6499a34e178a6e73feea92190e90e83d434f
v <umar2le@gmail.com>
2025-07-10 19:06:16 -0700

    Updated Uma PR

---
+++ src/js/data/ranking_data/Uma.js
@@ -0,0 +1,530 @@
+export const Uma = [
+  {
+    name: "\u221e by Tomoyo Takayanagi",
+    videoId: "https://naedist.animemusicquiz.com/iuj45q.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "Uma"
+      ]
+    },
+    sheetId: 1
+  },
+  {
+    name: "BRIGHTEST HEART by Tomoyo Takayanagi",
+    videoId: "https://naedist.animemusicquiz.com/73a74v.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "Uma"
+      ]
+    },
+    sheetId: 2
+  },
+  {
+    name: "Koeru by [Alexandros]",
+    videoId: "https://naedist.animemusicquiz.com/2tw61w.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "Uma"
+      ]
+    },
+    sheetId: 3
+  },
+  {
+    name: "Arigatou, Kamisama by Azumi Waki",
+    videoId: "https://naedist.animemusicquiz.com/3d4e1j.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "Uma"
+      ]
+    },
+    sheetId: 4
+  },
+  {
+    name: "Find My Only Way by Spica",
+    videoId: "https://naedist.animemusicquiz.com/px13cw.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "Uma"
+      ]
+    },
+    sheetId: 5
+  },
+  {
+    name: "Grow Up Shine! by Azumi Waki, Marika Kohno, Machico, Ayaka Ohashi, Chisa Kimura, Hitomi Ueda, Saori Oonishi",
+    videoId: "https://naedist.animemusicquiz.com/9egnoo.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "Uma"
+      ]
+    },
+    sheetId: 6
+  },
+  {
+    name: "Make debut! by Azumi Waki, Marika Kohno, Machico, Ayaka Ohashi, Chisa Kimura, Hitomi Ueda, Saori Oonishi",
+    videoId: "https://naedist.animemusicquiz.com/rt56h2.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "Uma"
+      ]
+    },
+    sheetId: 7
+  },
+  {
+    name: "Nana-iro no Keshiki by Marika Kohno",
+    videoId: "https://naedist.animemusicquiz.com/c4nj9p.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "Uma"
+      ]
+    },
+    sheetId: 8
+  },
+  {
+    name: "Silent Star by Marika Kohno",
+    videoId: "https://naedist.animemusicquiz.com/r26e7r.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "Uma"
+      ]
+    },
+    sheetId: 9
+  },
+  {
+    name: "Special Record! by Yui Kondou, Tomoyo Takayanagi, Spica, Rigil (without Yuka Ootsubo)",
+    videoId: "https://naedist.animemusicquiz.com/0cymtd.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "Uma"
+      ]
+    },
+    sheetId: 10
+  },
+  {
+    name: "Ambitious World by Hina Tachibana & Hinaki Yano",
+    videoId: "https://naedist.animemusicquiz.com/dqioic.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "Uma"
+      ]
+    },
+    sheetId: 11
+  },
+  {
+    name: "Enjoy and Join by Ikumi Hasegawa, Akari Kitou, Sumire Uesaka, Sora Tokui, Minami Tsuda, Hitomi Ueda, Keiko Watanabe",
+    videoId: "https://naedist.animemusicquiz.com/kxt93j.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "Uma"
+      ]
+    },
+    sheetId: 12
+  },
+  {
+    name: "Haruzora BLUE by Yui Watanabe, Keiko Watanabe, Yui Kondou",
+    videoId: "https://naedist.animemusicquiz.com/2emm8a.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "Uma"
+      ]
+    },
+    sheetId: 13
+  },
+  {
+    name: "Glorious Moment! by Kanna Nakamura, Hitomi Sasaki, Sora Tokui",
+    videoId: "https://naedist.animemusicquiz.com/dh946k.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "Uma"
+      ]
+    },
+    sheetId: 14
+  },
+  {
+    name: "Ichibanboshi ga Kakeru Sora by Kanna Nakamura, Hitomi Sasaki, Sora Tokui",
+    videoId: "https://naedist.animemusicquiz.com/qg7lq9.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "Uma"
+      ]
+    },
+    sheetId: 15
+  },
+  {
+    name: "Beyond the Finale by Yuri Fujimoto",
+    videoId: "https://naedist.animemusicquiz.com/5znhq6.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "Uma"
+      ]
+    },
+    sheetId: 16
+  },
+  {
+    name: "PRISMATIC SPURT!!!! by Yuri Fujimoto, Sumire Uesaka, Yui Ogura, Haruna Fukushima",
+    videoId: "https://naedist.animemusicquiz.com/ivdwzk.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "Uma"
+      ]
+    },
+    sheetId: 17
+  },
+  {
+    name: "Ready!! Steady!! Derby!! by Yuri Fujimoto, Sumire Uesaka, Yui Ogura, Haruna Fukushima",
+    videoId: "https://naedist.animemusicquiz.com/ejd6ly.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "Uma"
+      ]
+    },
+    sheetId: 18
+  },
+  {
+    name: "Uma Pyoi Densetsu by Shinjidai Uma Musume",
+    videoId: "https://naedist.animemusicquiz.com/ndmisg.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "Uma"
+      ]
+    },
+    sheetId: 19
+  },
+  {
+    name: "Koi wa Derby\u2606 by Machico",
+    videoId: "https://naedist.animemusicquiz.com/kw1ei5.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "Uma"
+      ]
+    },
+    sheetId: 20
+  },
+  {
+    name: "Komorebi no Yell by Machico & Saori Oonishi",
+    videoId: "https://naedist.animemusicquiz.com/ozt1re.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "Uma"
+      ]
+    },
+    sheetId: 21
+  },
+  {
+    name: "Negai no Katachi by Machico",
+    videoId: "https://naedist.animemusicquiz.com/3h3z9l.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "Uma"
+      ]
+    },
+    sheetId: 22
+  },
+  {
+    name: "Sasayaka na Inori by Manaka Iwami",
+    videoId: "https://naedist.animemusicquiz.com/qbeufq.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "Uma"
+      ]
+    },
+    sheetId: 23
+  },
+  {
+    name: "winning the soul by Machico",
+    videoId: "https://naedist.animemusicquiz.com/3nid8a.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "Uma"
+      ]
+    },
+    sheetId: 24
+  },
+  {
+    name: "Yume o Kakeru! by Azumi Waki, Marika Kohno, Machico, Ayaka Ohashi, Chisa Kimura, Hitomi Ueda, Saori Oonishi",
+    videoId: "https://naedist.animemusicquiz.com/4rihvd.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "Uma"
+      ]
+    },
+    sheetId: 25
+  },
+  {
+    name: "Yume o Kakeru! by Machico, Yui Kondou, Kaori Maeda",
+    videoId: "https://naedist.animemusicquiz.com/exvlse.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "Uma"
+      ]
+    },
+    sheetId: 26
+  },
+  {
+    name: "Akogare Challenge Dash!! by Hinaki Yano, Azumi Waki, Marika Kohno, Machico, Ayaka Ohashi, Chisa Kimura, Hitomi Ueda, Saori Oonishi",
+    videoId: "https://naedist.animemusicquiz.com/bjps8i.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "Uma"
+      ]
+    },
+    sheetId: 27
+  },
+  {
+    name: "Hikari no Ushirosugata by Yuuko Natsuyoshi",
+    videoId: "https://naedist.animemusicquiz.com/zardy9.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "Uma"
+      ]
+    },
+    sheetId: 28
+  },
+  {
+    name: "Lost Shine by Hinaki Yano",
+    videoId: "https://naedist.animemusicquiz.com/17f3cy.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "Uma"
+      ]
+    },
+    sheetId: 29
+  },
+  {
+    name: "Sora no Hohoemikata by Hinaki Yano",
+    videoId: "https://naedist.animemusicquiz.com/u3ofir.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "Uma"
+      ]
+    },
+    sheetId: 30
+  },
+  {
+    name: "Soshite Minna no by Hinaki Yano & Yuuko Natsuyoshi",
+    videoId: "https://naedist.animemusicquiz.com/bmvf0r.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "Uma"
+      ]
+    },
+    sheetId: 31
+  },
+  {
+    name: "Soshite Minna no by Hinaki Yano, Hina Tachibana, Sayumi Suzushiro, Yuuko Natsuyoshi, MAKIKO, Akina",
+    videoId: "https://naedist.animemusicquiz.com/eur9tk.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "Uma"
+      ]
+    },
+    sheetId: 32
+  },
+  {
+    name: "Yume no Kotae by Hina Tachibana",
+    videoId: "https://naedist.animemusicquiz.com/yqwnuw.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "Uma"
+      ]
+    },
+    sheetId: 33
+  },
+  {
+    name: "Nigekiri! Fallin' Love by Marika Kohno, Hitomi Owada, Ikumi Hasegawa, Lynn, Tomomi Mineuchi",
+    videoId: "https://naedist.animemusicquiz.com/58qm66.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "Uma"
+      ]
+    },
+    sheetId: 34
+  },
+  {
+    name: "Opera Gekijou \"Aa Sore ga Waga Sadame\" by Sora Tokui, Eriko Matsui, Lynn, Hitomi Owada",
+    videoId: "https://naedist.animemusicquiz.com/ac2ut9.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "Uma"
+      ]
+    },
+    sheetId: 35
+  },
+  {
+    name: "Pyoitto\u266aHallelujah! by Azumi Waki, Akari Kitou, Iori Saeki, Minami Takahashi, Rena Maeda",
+    videoId: "https://naedist.animemusicquiz.com/29kldk.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "Uma"
+      ]
+    },
+    sheetId: 36
+  },
+  {
+    name: "Ring Ring Diary by Machico, Azusa Tadokoro, Ruriko Aoki, Eriko Matsui, Yuiko Tatsumi",
+    videoId: "https://naedist.animemusicquiz.com/i5mlym.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "Uma"
+      ]
+    },
+    sheetId: 37
+  },
+  {
+    name: "#Uma RAP by Rena Maeda, Sora Tokui, Nozomi Yamamoto, Yui Watanabe, Keiko Watanabe",
+    videoId: "https://naedist.animemusicquiz.com/9opgl3.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "Uma"
+      ]
+    },
+    sheetId: 38
+  },
+  {
+    name: "Bright Melody by Ayaka Ohashi, Azusa Tadokoro, Ruriko Aoki, Minori Suzuki, Misato Matsuoka",
+    videoId: "https://naedist.animemusicquiz.com/sqs894.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "Uma"
+      ]
+    },
+    sheetId: 39
+  },
+  {
+    name: "Dance 2 Endless Beat by Lynn, Chisa Kimura, Minami Tsuda, Miharu Hanai, Miyu Yoshizaki",
+    videoId: "https://naedist.animemusicquiz.com/574ykz.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "Uma"
+      ]
+    },
+    sheetId: 40
+  },
+  {
+    name: "Eien no Shikisai by Azumi Waki, Tomoyo Takayanagi, Chinami Hashimoto, Minami Tsuda, Hikaru Toono",
+    videoId: "https://naedist.animemusicquiz.com/fvpegx.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "Uma"
+      ]
+    },
+    sheetId: 41
+  },
+  {
+    name: "Midnight Epilogue by Machico, Saori Oonishi, Sumire Uesaka, Hiyori Nitta, Miharu Hanai",
+    videoId: "https://naedist.animemusicquiz.com/muq4aw.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "Uma"
+      ]
+    },
+    sheetId: 42
+  },
+  {
+    name: "Umayuru Rap de Put Your Hands Up by Rena Maeda, Sora Tokui, Nozomi Yamamoto, Yui Watanabe, Keiko Watanabe",
+    videoId: "https://naedist.animemusicquiz.com/sfywrk.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "Uma"
+      ]
+    },
+    sheetId: 43
+  },
+  {
+    name: "Yuru Paka HAPPY DAYS! by Ayaka Ohashi, Chisa Kimura, Yoshino Aoyama, Meiku Harukawa, Misato Matsuoka",
+    videoId: "https://naedist.animemusicquiz.com/6hx1tp.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "Uma"
+      ]
+    },
+    sheetId: 44
+  },
+  {
+    name: "Hai no memoire by Akari Kitou",
+    videoId: "https://naedist.animemusicquiz.com/l5b1wn.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "Uma"
+      ]
+    },
+    sheetId: 45
+  },
+  {
+    name: "Hai no memoire by Tomoyo Takayanagi, Hitomi Ueda, Saori Oonishi, Naomi Oozora",
+    videoId: "https://naedist.animemusicquiz.com/4vqp2p.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "Uma"
+      ]
+    },
+    sheetId: 46
+  },
+  {
+    name: "Hai no memoire by Yui Kondou & Sakura Kasuga",
+    videoId: "https://naedist.animemusicquiz.com/1slwwt.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "Uma"
+      ]
+    },
+    sheetId: 47
+  },
+  {
+    name: "Hai no memoire by Yuu Sasahara & Hiyori Manase",
+    videoId: "https://naedist.animemusicquiz.com/9lov50.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "Uma"
+      ]
+    },
+    sheetId: 48
+  }
+];
