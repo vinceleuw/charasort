fd7ff056aca44b661fabaf028d1af03ba31dc6b5
v <umar2le@gmail.com>
2025-10-04 21:51:55 -0700

    added kfc nep 1

---
+++ src/js/data/ranking_data/KFCNep1.js
@@ -0,0 +1,266 @@
+export const KFCNep1 = [
+  {
+    name: "Boku Ga Shinou To Omottanowa",
+    videoId: "0HTAKT-JIaA",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep1"
+      ]
+    },
+    sheetId: 1
+  },
+  {
+    name: "Charm",
+    videoId: "Wkb5bznENiQ",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep1"
+      ]
+    },
+    sheetId: 2
+  },
+  {
+    name: "Chica Chica",
+    videoId: "9kmBLK8P2fE",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep1"
+      ]
+    },
+    sheetId: 3
+  },
+  {
+    name: "Congrats!! (with Friends)",
+    videoId: "0227xrfS65o",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep1"
+      ]
+    },
+    sheetId: 4
+  },
+  {
+    name: "Daisy",
+    videoId: "Cozx84HfG6w",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep1"
+      ]
+    },
+    sheetId: 5
+  },
+  {
+    name: "Dry Flower",
+    videoId: "kzZ6KXDM1RI",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep1"
+      ]
+    },
+    sheetId: 6
+  },
+  {
+    name: "Eiyuu",
+    videoId: "HZQX4qm8gnI",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep1"
+      ]
+    },
+    sheetId: 7
+  },
+  {
+    name: "FaV -F**** and Vanguard- ",
+    videoId: "u1lJgxCn8uY",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep1"
+      ]
+    },
+    sheetId: 8
+  },
+  {
+    name: "I'm a mess",
+    videoId: "ma4lm5eAdvg",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep1"
+      ]
+    },
+    sheetId: 9
+  },
+  {
+    name: "Kireigoto",
+    videoId: "VPhLXeU25KA",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep1"
+      ]
+    },
+    sheetId: 10
+  },
+  {
+    name: "Kyoukankaku Obake",
+    videoId: "-2iHNgLJHk8",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep1"
+      ]
+    },
+    sheetId: 11
+  },
+  {
+    name: "late in autumn",
+    videoId: "OA5F4dqEtmg",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep1"
+      ]
+    },
+    sheetId: 12
+  },
+  {
+    name: "Mariage",
+    videoId: "Cf_RvhLAXkk",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep1"
+      ]
+    },
+    sheetId: 13
+  },
+  {
+    name: "Message Bluem",
+    videoId: "wEjKXttbdBw",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep1"
+      ]
+    },
+    sheetId: 14
+  },
+  {
+    name: "Muteki no Soldier",
+    videoId: "Vmiij1J4mIw",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep1"
+      ]
+    },
+    sheetId: 15
+  },
+  {
+    name: "ODD",
+    videoId: "xotIC4YgxAo",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep1"
+      ]
+    },
+    sheetId: 16
+  },
+  {
+    name: "Odoriko",
+    videoId: "7HgJIAUtICU",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep1"
+      ]
+    },
+    sheetId: 17
+  },
+  {
+    name: "Paranoia",
+    videoId: "-fz_cIXi8OY",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep1"
+      ]
+    },
+    sheetId: 18
+  },
+  {
+    name: "PHOENIX",
+    videoId: "dZO-IOpx1VY",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep1"
+      ]
+    },
+    sheetId: 19
+  },
+  {
+    name: "Starry Jet",
+    videoId: "rth4htqJ1ZQ",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep1"
+      ]
+    },
+    sheetId: 20
+  },
+  {
+    name: "W/X/Y",
+    videoId: "fmDHfS0fXm4",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep1"
+      ]
+    },
+    sheetId: 21
+  },
+  {
+    name: "Waiting for Rain",
+    videoId: "766qmHTc2ro",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep1"
+      ]
+    },
+    sheetId: 22
+  },
+  {
+    name: "Zettai Zetsumei",
+    videoId: "DR5FSueiJxI",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep1"
+      ]
+    },
+    sheetId: 23
+  },
+  {
+    name: "\u03c9 Neko",
+    videoId: "V4nwy5IUMdg",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep1"
+      ]
+    },
+    sheetId: 24
+  }
+];
