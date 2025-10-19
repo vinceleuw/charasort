517337a0bcb5008b472a32540e0c9314c3b25d51
v <umar2le@gmail.com>
2025-10-19 13:27:37 -0700

    Add KFCNep2 PR

---
+++ src/js/data/ranking_data/KFCNep2.js
@@ -0,0 +1,266 @@
+export const KFCNep2 = [
+  {
+    name: "-Zero",
+    videoId: "LiD8u0dv808",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep2"
+      ]
+    },
+    sheetId: 1
+  },
+  {
+    name: "Ano Hi no Koi",
+    videoId: "5ITVZwrPr74",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep2"
+      ]
+    },
+    sheetId: 2
+  },
+  {
+    name: "Baka Mitai",
+    videoId: "g3jCAyPai2Y",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep2"
+      ]
+    },
+    sheetId: 3
+  },
+  {
+    name: "brave climber",
+    videoId: "vIdL68KNRMI",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep2"
+      ]
+    },
+    sheetId: 4
+  },
+  {
+    name: "Crystallize",
+    videoId: "uqVEPxflWyo",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep2"
+      ]
+    },
+    sheetId: 5
+  },
+  {
+    name: "Drops",
+    videoId: "6nda3Rlay28",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep2"
+      ]
+    },
+    sheetId: 6
+  },
+  {
+    name: "Endless Story",
+    videoId: "peJg9IxA87o",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep2"
+      ]
+    },
+    sheetId: 7
+  },
+  {
+    name: "Guide",
+    videoId: "MkHaKj-P7jA",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep2"
+      ]
+    },
+    sheetId: 8
+  },
+  {
+    name: "isolation",
+    videoId: "LS-3U5-O6EA",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep2"
+      ]
+    },
+    sheetId: 9
+  },
+  {
+    name: "Kibou Refrain",
+    videoId: "mAjkW6FoFVU",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep2"
+      ]
+    },
+    sheetId: 10
+  },
+  {
+    name: "Mambo Mambo Omatsuri Mambo",
+    videoId: "GmGmg0Oi2eE",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep2"
+      ]
+    },
+    sheetId: 11
+  },
+  {
+    name: "Motto ne!",
+    videoId: "YoYVNh9WgnM",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep2"
+      ]
+    },
+    sheetId: 12
+  },
+  {
+    name: "noche blanca",
+    videoId: "aGIFQsO1PDM",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep2"
+      ]
+    },
+    sheetId: 13
+  },
+  {
+    name: "Re:fresh",
+    videoId: "zpPbQQSYfFk",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep2"
+      ]
+    },
+    sheetId: 14
+  },
+  {
+    name: "Searchlight to Tsukiakari",
+    videoId: "k5GNbI9rqCg",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep2"
+      ]
+    },
+    sheetId: 15
+  },
+  {
+    name: "Sendan Life",
+    videoId: "qu1fnhI65mk",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep2"
+      ]
+    },
+    sheetId: 16
+  },
+  {
+    name: "sky fish",
+    videoId: "GULaOyT0jVk",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep2"
+      ]
+    },
+    sheetId: 17
+  },
+  {
+    name: "Slow Regret",
+    videoId: "H0Lhcslf2BM",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep2"
+      ]
+    },
+    sheetId: 18
+  },
+  {
+    name: "Star Trail",
+    videoId: "GOpf0Jp2c9U",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep2"
+      ]
+    },
+    sheetId: 19
+  },
+  {
+    name: "Tomoshibi no Manimani",
+    videoId: "tXsqImRzm80",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep2"
+      ]
+    },
+    sheetId: 20
+  },
+  {
+    name: "Tsubasa",
+    videoId: "s9u7oQ7aaV0",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep2"
+      ]
+    },
+    sheetId: 21
+  },
+  {
+    name: "Tsubasa to Kokuhaku",
+    videoId: "62cIKwrNUDk",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep2"
+      ]
+    },
+    sheetId: 22
+  },
+  {
+    name: "White Eternity",
+    videoId: "NarVowwWS_Q",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep2"
+      ]
+    },
+    sheetId: 23
+  },
+  {
+    name: "Yakudo",
+    videoId: "xJwrxGy_CpE",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep2"
+      ]
+    },
+    sheetId: 24
+  }
+];
