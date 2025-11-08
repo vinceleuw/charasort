2e6bc7ffe5803419b674de044f33c153487840bd
v <umar2le@gmail.com>
2025-11-07 23:18:16 -0800

    Added KFCNep3

---
+++ src/js/data/ranking_data/KFCNep3.js
@@ -0,0 +1,266 @@
+export const KFCNep3 = [
+  {
+    name: "8.32",
+    videoId: "l59GHITnuKE",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep3"
+      ]
+    },
+    sheetId: 1
+  },
+  {
+    name: "Ai Uta",
+    videoId: "5LLu-WKm74I",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep3"
+      ]
+    },
+    sheetId: 2
+  },
+  {
+    name: "Blooming in the Mud",
+    videoId: "08J6WAmX100",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep3"
+      ]
+    },
+    sheetId: 3
+  },
+  {
+    name: "Caramel Heaven",
+    videoId: "zzPowCFNIJM",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep3"
+      ]
+    },
+    sheetId: 4
+  },
+  {
+    name: "ERROR",
+    videoId: "eNlXPUp9WBw",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep3"
+      ]
+    },
+    sheetId: 5
+  },
+  {
+    name: "Fairytale,",
+    videoId: "uH40ob0Ewv8",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep3"
+      ]
+    },
+    sheetId: 6
+  },
+  {
+    name: "Hana ni Ame o, Kimi ni Uta o",
+    videoId: "x0zfQrIj_Ho",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep3"
+      ]
+    },
+    sheetId: 7
+  },
+  {
+    name: "Haruyo Koi",
+    videoId: "PKARvOtyftU",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep3"
+      ]
+    },
+    sheetId: 8
+  },
+  {
+    name: "Inochi ni Kirawarete iru.",
+    videoId: "x5MtkIi4uZY",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep3"
+      ]
+    },
+    sheetId: 9
+  },
+  {
+    name: "JANE DOE",
+    videoId: "U95v1zN5-gQ",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep3"
+      ]
+    },
+    sheetId: 10
+  },
+  {
+    name: "Kasou Kyousoukyoku",
+    videoId: "FCprxX-V9M8",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep3"
+      ]
+    },
+    sheetId: 11
+  },
+  {
+    name: "Kataomoi",
+    videoId: "fbvvS8e1KgI",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep3"
+      ]
+    },
+    sheetId: 12
+  },
+  {
+    name: "Kousui",
+    videoId: "3ATRMxqyjKg",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep3"
+      ]
+    },
+    sheetId: 13
+  },
+  {
+    name: "Love Story wa Totsuzenni",
+    videoId: "FdCWgBpfePo",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep3"
+      ]
+    },
+    sheetId: 14
+  },
+  {
+    name: "Mela!",
+    videoId: "aXDGqawZnd4",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep3"
+      ]
+    },
+    sheetId: 15
+  },
+  {
+    name: "Miiro",
+    videoId: "tdRngZ3cazk",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep3"
+      ]
+    },
+    sheetId: 16
+  },
+  {
+    name: "Rain stops, good-bye",
+    videoId: "aInQc9dLq_c",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep3"
+      ]
+    },
+    sheetId: 17
+  },
+  {
+    name: "Shinai",
+    videoId: "c8WEfFGBAPI",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep3"
+      ]
+    },
+    sheetId: 18
+  },
+  {
+    name: "Sleep",
+    videoId: "F0ptlgaeaMg",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep3"
+      ]
+    },
+    sheetId: 19
+  },
+  {
+    name: "The Noise of Rain",
+    videoId: "0VSFpqOjAkY",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep3"
+      ]
+    },
+    sheetId: 20
+  },
+  {
+    name: "Trancing Pulse",
+    videoId: "XO51HlD28W0",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep3"
+      ]
+    },
+    sheetId: 21
+  },
+  {
+    name: "Uma Pyoi Densetsu",
+    videoId: "aUitXsX5M-k",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep3"
+      ]
+    },
+    sheetId: 22
+  },
+  {
+    name: "Weight of the World",
+    videoId: "0PnrK9EXb1M",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep3"
+      ]
+    },
+    sheetId: 23
+  },
+  {
+    name: "Zutto Kimi no Soba de",
+    videoId: "YpuWpFihs-8",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep3"
+      ]
+    },
+    sheetId: 24
+  }
+];
