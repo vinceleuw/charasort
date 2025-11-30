0cdd7c6ae6061879e905e63471cd927868118ff2
v <umar2le@gmail.com>
2025-11-30 12:38:37 -0800

    Added To be hero x

---
+++ src/js/data/ranking_data/ToBeHeroX.js
@@ -0,0 +1,222 @@
+export const ToBeHeroX = [
+  {
+    name: "INERTIA by SawanoHiroyuki[nZk]:Rei",
+    videoId: "https://naedist.animemusicquiz.com/2jhx3w.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "ToBeHeroX"
+      ]
+    },
+    sheetId: 1
+  },
+  {
+    name: "PARAGON by Benjamin & mpi",
+    videoId: "https://naedist.animemusicquiz.com/fczxsu.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "ToBeHeroX"
+      ]
+    },
+    sheetId: 2
+  },
+  {
+    name: "KONTINUUM by SennaRin",
+    videoId: "https://naedist.animemusicquiz.com/8yxuox.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "ToBeHeroX"
+      ]
+    },
+    sheetId: 3
+  },
+  {
+    name: "NEON RAIN by Eliana",
+    videoId: "https://naedist.animemusicquiz.com/0i6u28.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "ToBeHeroX"
+      ]
+    },
+    sheetId: 4
+  },
+  {
+    name: "INERTIA by SawanoHiroyuki[nZk]:Rei & SennaRin",
+    videoId: "https://naedist.animemusicquiz.com/j6aja7z5fqktd47l.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "ToBeHeroX"
+      ]
+    },
+    sheetId: 5
+  },
+  {
+    name: "JEOPARDY by Benjamin & mpi",
+    videoId: "https://naedist.animemusicquiz.com/mg2bv1.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "ToBeHeroX"
+      ]
+    },
+    sheetId: 6
+  },
+  {
+    name: "Take Off by Lucky Cyan",
+    videoId: "https://naedist.animemusicquiz.com/eo2v2o.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "ToBeHeroX"
+      ]
+    },
+    sheetId: 7
+  },
+  {
+    name: "My Color by Sachi Kokuryu",
+    videoId: "https://naedist.animemusicquiz.com/24iuda.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "ToBeHeroX"
+      ]
+    },
+    sheetId: 8
+  },
+  {
+    name: "My Color by Lucky Cyan",
+    videoId: "https://naedist.animemusicquiz.com/n90yquwe16mxdsph.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "ToBeHeroX"
+      ]
+    },
+    sheetId: 9
+  },
+  {
+    name: "Queen of the Stars by R!N",
+    videoId: "https://naedist.animemusicquiz.com/5sqlvvbtgp5naii5.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "ToBeHeroX"
+      ]
+    },
+    sheetId: 10
+  },
+  {
+    name: "Candy Mountain by Kaon Kokudo",
+    videoId: "https://naedist.animemusicquiz.com/84mjiofcuccby20b.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "ToBeHeroX"
+      ]
+    },
+    sheetId: 11
+  },
+  {
+    name: "Because of you by Kris Roche",
+    videoId: "https://naedist.animemusicquiz.com/lvkickhvv6oppssv.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "ToBeHeroX"
+      ]
+    },
+    sheetId: 12
+  },
+  {
+    name: "growL by J. Rio",
+    videoId: "https://naedist.animemusicquiz.com/r4c1to3ftj5wl5m4.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "ToBeHeroX"
+      ]
+    },
+    sheetId: 13
+  },
+  {
+    name: "Scar by AK Liu Zhang",
+    videoId: "https://naedist.animemusicquiz.com/rv763u1acocxspvy.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "ToBeHeroX"
+      ]
+    },
+    sheetId: 14
+  },
+  {
+    name: "SPARX by Robert Taira Wilson",
+    videoId: "https://naedist.animemusicquiz.com/jcbo70wlppi0dolv.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "ToBeHeroX"
+      ]
+    },
+    sheetId: 15
+  },
+  {
+    name: "New Type of Hero by Chatterbox",
+    videoId: "https://naedist.animemusicquiz.com/uh8cxklumfh9kwhh.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "ToBeHeroX"
+      ]
+    },
+    sheetId: 16
+  },
+  {
+    name: "Wishing by Lucky Cyan",
+    videoId: "https://naedist.animemusicquiz.com/rzbzm3n089cwpslw.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "ToBeHeroX"
+      ]
+    },
+    sheetId: 17
+  },
+  {
+    name: "Twilight by Lucky Cyan",
+    videoId: "https://naedist.animemusicquiz.com/2cawz9v6dqvfq8c5.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "ToBeHeroX"
+      ]
+    },
+    sheetId: 18
+  },
+  {
+    name: "a Song of Journey by Kris Roche",
+    videoId: "https://naedist.animemusicquiz.com/pa1xiou5fkyq7l02.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "ToBeHeroX"
+      ]
+    },
+    sheetId: 19
+  },
+  {
+    name: "Light in the Shadows by Salajin",
+    videoId: "https://naedist.animemusicquiz.com/vr9m2oseykj6n5zd.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "ToBeHeroX"
+      ]
+    },
+    sheetId: 20
+  }
+];
