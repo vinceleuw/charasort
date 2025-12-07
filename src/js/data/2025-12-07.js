a5a6275397768f1d27ee044e4c77743eecb85cb7
v <umar2le@gmail.com>
2025-12-07 12:06:08 -0800

    added KFCNep4

---
+++ src/js/data/2025-12-07.js
@@ -0,0 +1,68 @@
+dataSetVersion = "2025-12-07"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSet[dataSetVersion] = {};
+
+import { AnonT100 } from "./ranking_data/AnonT100.js";
+import { Covers } from "./ranking_data/covers.js";
+import { SBR } from "./ranking_data/SBR.js";
+import { Sp2024OPS } from "./ranking_data/Sp2024OPS.js";
+import { Sp2024EDS } from "./ranking_data/Sp2024EDS.js";
+import { SeismicNonAni3 } from "./ranking_data/SeismicNonAni3.js";
+import { Su2024OPS } from "./ranking_data/Su2024OPS.js";
+import { Su2024EDS } from "./ranking_data/Su2024EDS.js";
+import { LoveLive } from "./ranking_data/LoveLive.js";
+import { EggsOnePiece } from "./ranking_data/EggsOnePiece.js";
+import { Uma } from "./ranking_data/Uma.js";
+import { NisioIsin } from "./ranking_data/NisioIsin.js";
+import { KFCNep1 } from "./ranking_data/KFCNep1.js";
+import { KFCNep2 } from "./ranking_data/KFCNep2.js";
+import { KFCNep3 } from "./ranking_data/KFCNep3.js";
+import { ToBeHeroX } from "./ranking_data/ToBeHeroX.js";
+import { KFCNep4 } from "./ranking_data/KFCNep4.js";
+
+dataSet[dataSetVersion].options = [
+  {
+    name: "Filter by Ranking",
+    key: "ranking",
+    tooltip: "Check this to restrict to certain series.",
+    checked: true,
+    sub: [
+      { name: "Covers", key: "Covers", checked: false},
+      { name: "Show By Rock!!", key: "SBR!!", checked: false},
+      { name: "Spring 2024 Openings", key: "Sp2024OPS", checked: false},
+      { name: "Spring 2024 Endings", key: "Sp2024EDS", checked: false},
+      { name: "Seismic Non-Ani 3", key: "SeismicNonAni3", checked: false},
+      { name: "Summer 2024 Openings", key: "Su2024OPS", checked: false},
+      { name: "Summer 2024 Endings", key: "Su2024EDS", checked: false},
+      { name: "Anon Top 100", key: "AnonT100", checked: false},
+      { name: "Love Live", key: "LoveLive", checked: false},
+      { name: "Eggs One Piece", key: "EggsOnePiece", checked: false},
+      { name: "Uma", key: "Uma", checked: false},
+      { name: "Nisio Isin", key: "Nisio Isin", checked: false},
+      { name: "KFC Nep 1", key: "KFCNep1", checked: false},
+      { name: "KFC Nep 2", key: "KFCNep2", checked: false},
+      { name: "KFC Nep 3", key: "KFCNep3", checked: false},
+      { name: "To Be Hero X", key: "ToBeHeroX", checked: false},
+      { name: "KFC Nep 4", key: "KFCNep4", checked: false}
+    ]
+  }
+];
+
+dataSet[dataSetVersion].characterData = [
+  ...Covers,
+  ...SBR,
+  ...Sp2024OPS,
+  ...Sp2024EDS,
+  ...SeismicNonAni3,
+  ...Su2024OPS,
+  ...Su2024EDS,
+  ...AnonT100,
+  ...LoveLive,
+  ...EggsOnePiece,
+  ...Uma,
+  ...NisioIsin,
+  ...KFCNep1,
+  ...KFCNep2,
+  ...KFCNep3,
+  ...ToBeHeroX,
+  ...KFCNep4
+];
---
+++ src/js/data/ranking_data/KFCNep4.js
@@ -0,0 +1,266 @@
+export const KFCNep4 = [
+  {
+    name: "Ano Natsu no Itsuka wa",
+    videoId: "igg0iHG8MR4",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep4"
+      ]
+    },
+    sheetId: 1
+  },
+  {
+    name: "Anokanatarium",
+    videoId: "bkO954_QPBs",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep4"
+      ]
+    },
+    sheetId: 2
+  },
+  {
+    name: "Before I Rise",
+    videoId: "ReJsfzGeIXE",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep4"
+      ]
+    },
+    sheetId: 3
+  },
+  {
+    name: "BETELGEUSE",
+    videoId: "rNiD3CMe1Qo",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep4"
+      ]
+    },
+    sheetId: 4
+  },
+  {
+    name: "Holiday\u221eHoliday",
+    videoId: "IHSUgM4rVQg",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep4"
+      ]
+    },
+    sheetId: 5
+  },
+  {
+    name: "Hoshikuzu Utopia",
+    videoId: "7H5RwhKMMWI",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep4"
+      ]
+    },
+    sheetId: 6
+  },
+  {
+    name: "Ikanaide",
+    videoId: "mUeLFVt9FEg",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep4"
+      ]
+    },
+    sheetId: 7
+  },
+  {
+    name: "Kami no Manimani",
+    videoId: "c8_d0ngKtfo",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep4"
+      ]
+    },
+    sheetId: 8
+  },
+  {
+    name: "Key of Truth",
+    videoId: "b9hT8BHJ7VE",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep4"
+      ]
+    },
+    sheetId: 9
+  },
+  {
+    name: "Kimi ga Kureta Oto",
+    videoId: "z4nCPYVjPM8",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep4"
+      ]
+    },
+    sheetId: 10
+  },
+  {
+    name: "Koi Yuki",
+    videoId: "gz0nBKnb-ao",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep4"
+      ]
+    },
+    sheetId: 11
+  },
+  {
+    name: "Love Destiny",
+    videoId: "99vsDduk0lU",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep4"
+      ]
+    },
+    sheetId: 12
+  },
+  {
+    name: "Mezameta Asa ni wa Kimi ga Tonari ni",
+    videoId: "nd_PQNuT0DQ",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep4"
+      ]
+    },
+    sheetId: 13
+  },
+  {
+    name: "Nanairo no Tane",
+    videoId: "AHsVFym-yVU",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep4"
+      ]
+    },
+    sheetId: 14
+  },
+  {
+    name: "NATSUKAGE",
+    videoId: "O51h5Bn4bgc",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep4"
+      ]
+    },
+    sheetId: 15
+  },
+  {
+    name: "NAVILLERA",
+    videoId: "Se8bbsUFjC8",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep4"
+      ]
+    },
+    sheetId: 16
+  },
+  {
+    name: "Okibi",
+    videoId: "rn0EPL5c9Vc",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep4"
+      ]
+    },
+    sheetId: 17
+  },
+  {
+    name: "Overfly",
+    videoId: "Nmpz6RApirE",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep4"
+      ]
+    },
+    sheetId: 18
+  },
+  {
+    name: "Pasta no Uta ~Peperoncino~",
+    videoId: "wMc_VcjMhXw",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep4"
+      ]
+    },
+    sheetId: 19
+  },
+  {
+    name: "Sakurairo Time Capsule",
+    videoId: "knNu-qnWxLE",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep4"
+      ]
+    },
+    sheetId: 20
+  },
+  {
+    name: "Sis\u00d7Love",
+    videoId: "BfyH4ROtFm0",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep4"
+      ]
+    },
+    sheetId: 21
+  },
+  {
+    name: "Wait For You",
+    videoId: "xaHyvAMLk7U",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep4"
+      ]
+    },
+    sheetId: 22
+  },
+  {
+    name: "World Domination",
+    videoId: "QMGvSE9CyaY",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep4"
+      ]
+    },
+    sheetId: 23
+  },
+  {
+    name: "You are a ghost, I am a ghost ~Gekijou no Ghost~",
+    videoId: "R8zIk-x3DrA",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep4"
+      ]
+    },
+    sheetId: 24
+  }
+];
