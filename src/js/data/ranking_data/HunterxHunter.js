44d6b4ed1292d3be851182c32b63d53068acc832
v <umar2le@gmail.com>
2026-03-30 18:07:11 -0700

    Added HunterxHunter

---
+++ src/js/data/ranking_data/HunterxHunter.js
@@ -0,0 +1,211 @@
+export const HunterxHunter = [
+  {
+    name: "Ohayou. by Keno",
+    videoId: "https://naedist.animemusicquiz.com/g1nm3p.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "HunterxHunter"
+      ]
+    },
+    sheetId: 1
+  },
+  {
+    name: "Taiyou wa Yoru mo Kagayaku by WINO",
+    videoId: "https://naedist.animemusicquiz.com/mi6inl.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "HunterxHunter"
+      ]
+    },
+    sheetId: 2
+  },
+  {
+    name: "Kaze no Uta by Minako Honda",
+    videoId: "https://naedist.animemusicquiz.com/ditn30.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "HunterxHunter"
+      ]
+    },
+    sheetId: 3
+  },
+  {
+    name: "E-jan -Do You Feel Like I Feel? by Masato Nagai",
+    videoId: "https://naedist.animemusicquiz.com/fun5f2.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "HunterxHunter"
+      ]
+    },
+    sheetId: 4
+  },
+  {
+    name: "Hotaru by Masato Nagai",
+    videoId: "https://naedist.animemusicquiz.com/2l0h9j.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "HunterxHunter"
+      ]
+    },
+    sheetId: 5
+  },
+  {
+    name: "PALE ALE by Kenichi Kurosawa",
+    videoId: "https://naedist.animemusicquiz.com/1lzgwl.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "HunterxHunter"
+      ]
+    },
+    sheetId: 6
+  },
+  {
+    name: "Carry On by Kenichi Kurosawa",
+    videoId: "https://naedist.animemusicquiz.com/rch8xn.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "HunterxHunter"
+      ]
+    },
+    sheetId: 7
+  },
+  {
+    name: "Pray by Wish*",
+    videoId: "https://naedist.animemusicquiz.com/4tg71c.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "HunterxHunter"
+      ]
+    },
+    sheetId: 8
+  },
+  {
+    name: "POPCORN by Mikuni Shimokawa",
+    videoId: "https://naedist.animemusicquiz.com/nalobg.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "HunterxHunter"
+      ]
+    },
+    sheetId: 9
+  },
+  {
+    name: "Believe In Tomorrow by Sunflower's Garden",
+    videoId: "https://naedist.animemusicquiz.com/klo5in.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "HunterxHunter"
+      ]
+    },
+    sheetId: 10
+  },
+  {
+    name: "Moshimo Kono Sekai de Kimi to Boku ga Deaenakattara by Sunflower's Garden",
+    videoId: "https://naedist.animemusicquiz.com/f4nelg.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "HunterxHunter"
+      ]
+    },
+    sheetId: 11
+  },
+  {
+    name: "departure! by Masatoshi Ono",
+    videoId: "https://naedist.animemusicquiz.com/gomkdc.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "HunterxHunter"
+      ]
+    },
+    sheetId: 12
+  },
+  {
+    name: "departure! -second version- by Masatoshi Ono",
+    videoId: "https://naedist.animemusicquiz.com/a8ulv9.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "HunterxHunter"
+      ]
+    },
+    sheetId: 13
+  },
+  {
+    name: "Just Awake by Fear, and Loathing in Las Vegas",
+    videoId: "https://naedist.animemusicquiz.com/1c10sy.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "HunterxHunter"
+      ]
+    },
+    sheetId: 14
+  },
+  {
+    name: "HUNTING FOR YOUR DREAM by GALNERYUS",
+    videoId: "https://naedist.animemusicquiz.com/pwlu1q.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "HunterxHunter"
+      ]
+    },
+    sheetId: 15
+  },
+  {
+    name: "Riot by Tokyo Konsei Gasshou-dan",
+    videoId: "https://naedist.animemusicquiz.com/vjcwpp31kqr3r5fl.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "HunterxHunter"
+      ]
+    },
+    sheetId: 16
+  },
+  {
+    name: "REASON by Yuzu",
+    videoId: "https://naedist.animemusicquiz.com/5qo4z0.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "HunterxHunter"
+      ]
+    },
+    sheetId: 17
+  },
+  {
+    name: "Nagareboshi Kirari (Yuzu Version) by Yuzu",
+    videoId: "https://naedist.animemusicquiz.com/0bp729.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "HunterxHunter"
+      ]
+    },
+    sheetId: 18
+  },
+  {
+    name: "Hyouri Ittai by Yuzu",
+    videoId: "https://naedist.animemusicquiz.com/ca208v.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "HunterxHunter"
+      ]
+    },
+    sheetId: 19
+  }
+];
