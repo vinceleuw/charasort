325ad64e6f37dff17a636d17f9c6c0e138e72beb
v <umar2le@gmail.com>
2026-04-02 14:07:12 -0700

    Added AIra Yuuki & Nep 6

---
+++ src/js/data/ranking_data/AiraYuuki.js
@@ -0,0 +1,365 @@
+export const AiraYuuki = [
+  {
+    name: "everlasting song~japanese edition by FictionJunction ASUKA",
+    videoId: "https://naedist.animemusicquiz.com/i5l18n.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "AiraYuuki"
+      ]
+    },
+    sheetId: 1
+  },
+  {
+    name: "everlasting song~english edition by FictionJunction ASUKA",
+    videoId: "https://naedist.animemusicquiz.com/e3letc.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "AiraYuuki"
+      ]
+    },
+    sheetId: 2
+  },
+  {
+    name: "Sekai no Namida by Aira Yuuki",
+    videoId: "https://naedist.animemusicquiz.com/ro2bne.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "AiraYuuki"
+      ]
+    },
+    sheetId: 3
+  },
+  {
+    name: "Zankoku yo Kibou to Nare by Aira Yuuki",
+    videoId: "https://naedist.animemusicquiz.com/d4y1e9.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "AiraYuuki"
+      ]
+    },
+    sheetId: 4
+  },
+  {
+    name: "colorless wind by Aira Yuuki",
+    videoId: "https://naedist.animemusicquiz.com/n0th27.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "AiraYuuki"
+      ]
+    },
+    sheetId: 5
+  },
+  {
+    name: "Miageru Ano Sora de by Aira Yuuki",
+    videoId: "https://naedist.animemusicquiz.com/yrlcdh.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "AiraYuuki"
+      ]
+    },
+    sheetId: 6
+  },
+  {
+    name: "Moon Legend by Aira Yuuki",
+    videoId: "https://naedist.animemusicquiz.com/kxuy11.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "AiraYuuki"
+      ]
+    },
+    sheetId: 7
+  },
+  {
+    name: "Blue sky, True sky by Aira Yuuki",
+    videoId: "https://naedist.animemusicquiz.com/3kz0du.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "AiraYuuki"
+      ]
+    },
+    sheetId: 8
+  },
+  {
+    name: "Weeping alone by Aira Yuuki",
+    videoId: "https://naedist.animemusicquiz.com/vs13j2.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "AiraYuuki"
+      ]
+    },
+    sheetId: 9
+  },
+  {
+    name: "Dominant space by Aira Yuuki",
+    videoId: "https://naedist.animemusicquiz.com/9lnjfr.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "AiraYuuki"
+      ]
+    },
+    sheetId: 10
+  },
+  {
+    name: "LAMENT~Yagate Yorokobi o~ by Aira Yuuki",
+    videoId: "https://naedist.animemusicquiz.com/lc0ydq.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "AiraYuuki"
+      ]
+    },
+    sheetId: 11
+  },
+  {
+    name: "Hoshi o Motomete by Aira Yuuki",
+    videoId: "https://naedist.animemusicquiz.com/n1y8y6.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "AiraYuuki"
+      ]
+    },
+    sheetId: 12
+  },
+  {
+    name: "Uchuu Senkan Yamato by Project Yamato 2199",
+    videoId: "https://naedist.animemusicquiz.com/28h8yc.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "AiraYuuki"
+      ]
+    },
+    sheetId: 13
+  },
+  {
+    name: "Hoshi ga Towa o Terashiteru by Aira Yuuki",
+    videoId: "https://naedist.animemusicquiz.com/n525e8.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "AiraYuuki"
+      ]
+    },
+    sheetId: 14
+  },
+  {
+    name: "-Side SunSet- Kanashimi wa Dare no Negai demo Nai by Aira Yuuki",
+    videoId: "https://naedist.animemusicquiz.com/puv1dt.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "AiraYuuki"
+      ]
+    },
+    sheetId: 15
+  },
+  {
+    name: "Mada dame yo by FictionJunction ASUKA",
+    videoId: "https://naedist.animemusicquiz.com/6mqtd5.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "AiraYuuki"
+      ]
+    },
+    sheetId: 16
+  },
+  {
+    name: "REINCARNATION BLUE by Aira Yuuki",
+    videoId: "https://naedist.animemusicquiz.com/s41e37.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "AiraYuuki"
+      ]
+    },
+    sheetId: 17
+  },
+  {
+    name: "Mirai no Moyou by Aira Yuuki",
+    videoId: "https://naedist.animemusicquiz.com/0n0rbx.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "AiraYuuki"
+      ]
+    },
+    sheetId: 18
+  },
+  {
+    name: "Itoshiki Aragai yo, Michibike Hikari e by Aira Yuuki",
+    videoId: "https://naedist.animemusicquiz.com/drqh2t.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "AiraYuuki"
+      ]
+    },
+    sheetId: 19
+  },
+  {
+    name: "Pale Moon ga Yureteru by Aira Yuuki",
+    videoId: "https://naedist.animemusicquiz.com/4ufkea.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "AiraYuuki"
+      ]
+    },
+    sheetId: 20
+  },
+  {
+    name: "It's my life by Aira Yuuki",
+    videoId: "https://naedist.animemusicquiz.com/v0v9mg.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "AiraYuuki"
+      ]
+    },
+    sheetId: 21
+  },
+  {
+    name: "Donna Hoshizora yori mo, Donna Omoide yori mo (Piano Version) by Aira Yuuki",
+    videoId: "https://naedist.animemusicquiz.com/y2yrea.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "AiraYuuki"
+      ]
+    },
+    sheetId: 22
+  },
+  {
+    name: "Donna Hoshizora yori mo, Donna Omoide yori mo by Aira Yuuki",
+    videoId: "https://naedist.animemusicquiz.com/2q0unn.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "AiraYuuki"
+      ]
+    },
+    sheetId: 23
+  },
+  {
+    name: "Believe in... by Aira Yuuki",
+    videoId: "https://naedist.animemusicquiz.com/a5ewpp.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "AiraYuuki"
+      ]
+    },
+    sheetId: 24
+  },
+  {
+    name: "Violet Snow by Aira Yuuki",
+    videoId: "https://naedist.animemusicquiz.com/mhppuq.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "AiraYuuki"
+      ]
+    },
+    sheetId: 25
+  },
+  {
+    name: "Tada no Amaoto ni Naru made by Aira Yuuki",
+    videoId: "https://naedist.animemusicquiz.com/9jyyxp.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "AiraYuuki"
+      ]
+    },
+    sheetId: 26
+  },
+  {
+    name: "Blessing by Aira Yuuki",
+    videoId: "https://naedist.animemusicquiz.com/520xq6.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "AiraYuuki"
+      ]
+    },
+    sheetId: 27
+  },
+  {
+    name: "Pray by Aira Yuuki",
+    videoId: "https://naedist.animemusicquiz.com/vpqnve.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "AiraYuuki"
+      ]
+    },
+    sheetId: 28
+  },
+  {
+    name: "A Promise by Aira Yuuki",
+    videoId: "https://naedist.animemusicquiz.com/o8nczb.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "AiraYuuki"
+      ]
+    },
+    sheetId: 29
+  },
+  {
+    name: "Kakurenbo by Aira Yuuki",
+    videoId: "https://naedist.animemusicquiz.com/l2urk0.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "AiraYuuki"
+      ]
+    },
+    sheetId: 30
+  },
+  {
+    name: "Semisweet Afternoon by Aira Yuuki",
+    videoId: "https://naedist.animemusicquiz.com/an1rdg.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "AiraYuuki"
+      ]
+    },
+    sheetId: 31
+  },
+  {
+    name: "Blessing -realize- by Aira Yuuki",
+    videoId: "https://naedist.animemusicquiz.com/yjhebu.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "AiraYuuki"
+      ]
+    },
+    sheetId: 32
+  },
+  {
+    name: "sincerity flower by Aira Yuuki",
+    videoId: "https://naedist.animemusicquiz.com/qidpvwe2dimmvfzx.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "AiraYuuki"
+      ]
+    },
+    sheetId: 33
+  }
+];
---
+++ src/js/data/ranking_data/KFCNep6.js
@@ -0,0 +1,310 @@
+export const KFCNep6 = [
+  {
+    name: "Alice by IVVY",
+    videoId: "htjC7vhCnc8",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep6"
+      ]
+    },
+    sheetId: 1
+  },
+  {
+    name: "Anata No Koibito Ni Naritai by Ao WuWu",
+    videoId: "PVPLym_R78s",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep6"
+      ]
+    },
+    sheetId: 2
+  },
+  {
+    name: "Behavior by Kyo Hanabasami",
+    videoId: "6yZWJPndzLs",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep6"
+      ]
+    },
+    sheetId: 3
+  },
+  {
+    name: "Born This Way by Asaka",
+    videoId: "HtnS0-JftEc",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep6"
+      ]
+    },
+    sheetId: 4
+  },
+  {
+    name: "Calendar Girl by Kotoha, Megu, Hanon",
+    videoId: "0_vfdJ_M298",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep6"
+      ]
+    },
+    sheetId: 5
+  },
+  {
+    name: "Catch Me If You Can by Eileen Marine",
+    videoId: "Lixpftlm0Eo",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep6"
+      ]
+    },
+    sheetId: 6
+  },
+  {
+    name: "Cause your love ~Shiroi Melody~ by Satou Hiromi",
+    videoId: "U_Qa-P031Q4",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep6"
+      ]
+    },
+    sheetId: 7
+  },
+  {
+    name: "DROP by ClariS",
+    videoId: "1m0ITRv_R0w",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep6"
+      ]
+    },
+    sheetId: 8
+  },
+  {
+    name: "Frozen Eclipse by Airi Kanna",
+    videoId: "RbQv87f4mvg",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep6"
+      ]
+    },
+    sheetId: 9
+  },
+  {
+    name: "Happy to go! by Yuusha Party",
+    videoId: "iahH0CUlh-M",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep6"
+      ]
+    },
+    sheetId: 10
+  },
+  {
+    name: "Houseki wo Yadosu (feat. Akina) by Imy",
+    videoId: "rSDxc4zp7W4",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep6"
+      ]
+    },
+    sheetId: 11
+  },
+  {
+    name: "If ~Mata Aetara~ by DAY6",
+    videoId: "unS_PcMn3RA",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep6"
+      ]
+    },
+    sheetId: 12
+  },
+  {
+    name: "Invisible Rain by Minori Suzuki",
+    videoId: "j3qvPWAP9zA",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep6"
+      ]
+    },
+    sheetId: 13
+  },
+  {
+    name: "Kazatte by 25-ji, Nightcord de. feat. Kagamine Len",
+    videoId: "ab3ku_Zv7Jo",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep6"
+      ]
+    },
+    sheetId: 14
+  },
+  {
+    name: "Kimochi Kitto Tsunagaru yo by Wish me mell",
+    videoId: "HMm6YzVtDYY",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep6"
+      ]
+    },
+    sheetId: 15
+  },
+  {
+    name: "Kohaku by Mugendai Mewtype",
+    videoId: "dkeX5Qy_ths",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep6"
+      ]
+    },
+    sheetId: 16
+  },
+  {
+    name: "LUNDI by konoco",
+    videoId: "41EG3t-nwMY",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep6"
+      ]
+    },
+    sheetId: 17
+  },
+  {
+    name: "Marathon by Hanazawa Kana",
+    videoId: "6RUWFtDJbG0",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep6"
+      ]
+    },
+    sheetId: 18
+  },
+  {
+    name: "MEGITSUNE by BABYMETAL",
+    videoId: "cK3NMZAUKGw",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep6"
+      ]
+    },
+    sheetId: 19
+  },
+  {
+    name: "Memai memento by Ariabl'eyes feat. Risa Yuzuki",
+    videoId: "CLq01JgO5tA",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep6"
+      ]
+    },
+    sheetId: 20
+  },
+  {
+    name: "One Day by Omoinotake",
+    videoId: "HxedVG0rzA8",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep6"
+      ]
+    },
+    sheetId: 21
+  },
+  {
+    name: "Someone like U by Dalshabet",
+    videoId: "6H1QOFBNRWs",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep6"
+      ]
+    },
+    sheetId: 22
+  },
+  {
+    name: "Sora ga Matteru by HACHI",
+    videoId: "mZxjlMPEKd4",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep6"
+      ]
+    },
+    sheetId: 23
+  },
+  {
+    name: "Strawberry Rush by CHUU",
+    videoId: "JRbXa1w-Pa8",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep6"
+      ]
+    },
+    sheetId: 24
+  },
+  {
+    name: "Suicide by suisoh",
+    videoId: "OP_llyfMuI0",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep6"
+      ]
+    },
+    sheetId: 25
+  },
+  {
+    name: "Tell Your World by Saori Hayami",
+    videoId: "24OIBLd8uu8",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep6"
+      ]
+    },
+    sheetId: 26
+  },
+  {
+    name: "True Love by Sakura Yuno",
+    videoId: "1zdTQDfFMJU",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep6"
+      ]
+    },
+    sheetId: 27
+  },
+  {
+    name: "You May Not Want To Hear This But by Foreground Eclipse",
+    videoId: "5wSQCU3aSyc",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep6"
+      ]
+    },
+    sheetId: 28
+  }
+];
