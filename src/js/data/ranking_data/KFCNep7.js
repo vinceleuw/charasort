cfc5e2d760be3f5e3b76f274442752c15bb6d22a
v <umar2le@gmail.com>
2026-04-17 16:22:38 -0700

    Add Nep7

---
+++ src/js/data/ranking_data/KFCNep7.js
@@ -0,0 +1,310 @@
+export const KFCNep7 = [
+  {
+    name: "\u221e by HACHI",
+    videoId: "kfDHCH0o8MA",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep7"
+      ]
+    },
+    sheetId: 1
+  },
+  {
+    name: "9 wo Nagameta Sakanatachi by 957",
+    videoId: "nD_y3k_wvM0",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep7"
+      ]
+    },
+    sheetId: 2
+  },
+  {
+    name: "All for one Forever by Chihaya Yoshitake \u00b7 Karin Isobe \u00b7 Rie Kitagawa \u00b7 Yuri Komagata \u00b7 Machico \u00b7 Kanako Miyamoto",
+    videoId: "6j4Et9N17ns",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep7"
+      ]
+    },
+    sheetId: 3
+  },
+  {
+    name: "Ato Ippo by Jewel",
+    videoId: "nXNRq4uTsIw",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep7"
+      ]
+    },
+    sheetId: 4
+  },
+  {
+    name: "Become by Hebi",
+    videoId: "XPqXU3SfxdU",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep7"
+      ]
+    },
+    sheetId: 5
+  },
+  {
+    name: "Cherish (My Love) by ILLIT",
+    videoId: "tbDGl7jEazA",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep7"
+      ]
+    },
+    sheetId: 6
+  },
+  {
+    name: "eclipse by Mami Kawada",
+    videoId: "paYxMTpDjXU",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep7"
+      ]
+    },
+    sheetId: 7
+  },
+  {
+    name: "Euphorium by Suzuyu",
+    videoId: "fVsmCsDqbBM",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep7"
+      ]
+    },
+    sheetId: 8
+  },
+  {
+    name: "Hakujitsumu by Islet feat. isui",
+    videoId: "9k_csTdypRQ",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep7"
+      ]
+    },
+    sheetId: 9
+  },
+  {
+    name: "Hazakura by Kie Kitano",
+    videoId: "kQfqxMtF-GM",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep7"
+      ]
+    },
+    sheetId: 10
+  },
+  {
+    name: "Hide and Seek by isui",
+    videoId: "hKV4X4tVflA",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep7"
+      ]
+    },
+    sheetId: 11
+  },
+  {
+    name: "Hope Is the Thing With Feathers by Chevy",
+    videoId: "l8-v7ZnNB_c",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep7"
+      ]
+    },
+    sheetId: 12
+  },
+  {
+    name: "in the Garden by Primary",
+    videoId: "5Cof9rP7TEQ",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep7"
+      ]
+    },
+    sheetId: 13
+  },
+  {
+    name: "Indigo Star by Shiori Sasaki",
+    videoId: "F5Fo7Afz1mI",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep7"
+      ]
+    },
+    sheetId: 14
+  },
+  {
+    name: "Kaisei (Gekijouban Project Sekai) ver. by Leo/need",
+    videoId: "SzYVxmeFfFs",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep7"
+      ]
+    },
+    sheetId: 15
+  },
+  {
+    name: "Kofukuron by Moeka Koizumi",
+    videoId: "nTCiT6t__nw",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep7"
+      ]
+    },
+    sheetId: 16
+  },
+  {
+    name: "Kuusou Ressha by Startend",
+    videoId: "p1lfwyWSGV4",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep7"
+      ]
+    },
+    sheetId: 17
+  },
+  {
+    name: "Milk Crown on Sonnetica by Nameless",
+    videoId: "WG1lHyvPKZg",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep7"
+      ]
+    },
+    sheetId: 18
+  },
+  {
+    name: "Palette by Yuyoyuppe feat Hanatan",
+    videoId: "_Ay46hP7CjI",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep7"
+      ]
+    },
+    sheetId: 19
+  },
+  {
+    name: "Parade by Yorushika",
+    videoId: "ry3Tupx4BL4",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep7"
+      ]
+    },
+    sheetId: 20
+  },
+  {
+    name: "Promise by Raon Lee",
+    videoId: "HXp0D0vk4gg",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep7"
+      ]
+    },
+    sheetId: 21
+  },
+  {
+    name: "Re:TrymenT by Murasaki Hotaru",
+    videoId: "cT3wbp17Ano",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep7"
+      ]
+    },
+    sheetId: 22
+  },
+  {
+    name: "Sky Sanctuary by Miyuki Hashimoto",
+    videoId: "145Yjz6wFvM",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep7"
+      ]
+    },
+    sheetId: 23
+  },
+  {
+    name: "Star Trail by Ikumi Hasegawa",
+    videoId: "nxxQmi2eakM",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep7"
+      ]
+    },
+    sheetId: 24
+  },
+  {
+    name: "Suiheisen no you na Bokura e by Vivid Lila feat. Hana",
+    videoId: "JWX2eik5gRM",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep7"
+      ]
+    },
+    sheetId: 25
+  },
+  {
+    name: "Sweet Tack by Yanagi Nagi",
+    videoId: "satXR3_JFdg",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep7"
+      ]
+    },
+    sheetId: 26
+  },
+  {
+    name: "Tsuki no Yoru no Kemono-tachi by Lucia feat. Lukia",
+    videoId: "QmDqX_VvI1A",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep7"
+      ]
+    },
+    sheetId: 27
+  },
+  {
+    name: "Unveil by *Luna ft. Nenne",
+    videoId: "w5wNu69O2KA",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "KFCNep7"
+      ]
+    },
+    sheetId: 28
+  }
+];
