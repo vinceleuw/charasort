1d4485a7f74683507652fb242b80cf0bc490b60a
v <umar2le@gmail.com>
2026-06-02 21:37:58 -0700

    Added A

---
+++ src/js/data/ranking_data/A.js
@@ -0,0 +1,629 @@
+export const A = [
+  {
+    name: "\"Avid\" by SawanoHiroyuki[nZk]:mizuki",
+    videoId: "https://naedist.animemusicquiz.com/4s6bt7.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "A"
+      ]
+    },
+    sheetId: 1
+  },
+  {
+    name: "\"Absolute Soul\" by Konomi Suzuki",
+    videoId: "https://naedist.animemusicquiz.com/51o1kl.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "A"
+      ]
+    },
+    sheetId: 2
+  },
+  {
+    name: "\"Akatsuki\" by Akiko Shikata",
+    videoId: "https://naedist.animemusicquiz.com/rukz86ave6kvphlx.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "A"
+      ]
+    },
+    sheetId: 3
+  },
+  {
+    name: "\"Ave Mujica\" by Ave Mujica",
+    videoId: "https://naedist.animemusicquiz.com/qzvtj3.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "A"
+      ]
+    },
+    sheetId: 4
+  },
+  {
+    name: "\"ASEED\" by ZAQ",
+    videoId: "https://naedist.animemusicquiz.com/pi3172.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "A"
+      ]
+    },
+    sheetId: 5
+  },
+  {
+    name: "\"Aoi Tori\"  by fumika",
+    videoId: "https://naedist.animemusicquiz.com/7i0ff5.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "A"
+      ]
+    },
+    sheetId: 6
+  },
+  {
+    name: "\"After Dark\" by ASIAN KUNG-FU GENERATION",
+    videoId: "https://naedist.animemusicquiz.com/whhreo.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "A"
+      ]
+    },
+    sheetId: 7
+  },
+  {
+    name: "\"Ano Band\" by Kessoku Band",
+    videoId: "https://naedist.animemusicquiz.com/xqao00.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "A"
+      ]
+    },
+    sheetId: 8
+  },
+  {
+    name: "\"ANGER/ANGER\" by MYTH & ROID",
+    videoId: "https://naedist.animemusicquiz.com/abv5z0.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "A"
+      ]
+    },
+    sheetId: 9
+  },
+  {
+    name: "\"Ari no Mama Kimi no Mama\" by Noriko Noguchi",
+    videoId: "https://naedist.animemusicquiz.com/skw0xn.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "A"
+      ]
+    },
+    sheetId: 10
+  },
+  {
+    name: "\"Aoi Sangoshou\" by Seiko Matsuda",
+    videoId: "https://naedist.animemusicquiz.com/6kxrb1.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "A"
+      ]
+    },
+    sheetId: 11
+  },
+  {
+    name: "\"Aozora\" by miwa",
+    videoId: "https://naedist.animemusicquiz.com/5l2eu8.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "A"
+      ]
+    },
+    sheetId: 12
+  },
+  {
+    name: "\"A.I. am Human\" by MONKEY MAJIK",
+    videoId: "https://naedist.animemusicquiz.com/nsqra0.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "A"
+      ]
+    },
+    sheetId: 13
+  },
+  {
+    name: "\"Akatsuki\" by Photon Maiden",
+    videoId: "https://naedist.animemusicquiz.com/izs1ax.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "A"
+      ]
+    },
+    sheetId: 14
+  },
+  {
+    name: "\"agony\" by KOTOKO",
+    videoId: "https://naedist.animemusicquiz.com/brhx6t.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "A"
+      ]
+    },
+    sheetId: 15
+  },
+  {
+    name: "\"Aikotoba\" by Ayumi Miyazaki & AiM",
+    videoId: "https://naedist.animemusicquiz.com/f4zymx.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "A"
+      ]
+    },
+    sheetId: 16
+  },
+  {
+    name: "\"after laughter\" by school food punishment",
+    videoId: "https://naedist.animemusicquiz.com/s262sy.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "A"
+      ]
+    },
+    sheetId: 17
+  },
+  {
+    name: "\"Ashita o Narase\" by Shishido Kavka",
+    videoId: "https://files.catbox.moe/3betgd.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "A"
+      ]
+    },
+    sheetId: 18
+  },
+  {
+    name: "\"Ai no Uta -words of love-\" by Haruka Chisuga",
+    videoId: "https://naedist.animemusicquiz.com/b61fpy.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "A"
+      ]
+    },
+    sheetId: 19
+  },
+  {
+    name: "\"Adayume\" by MyGO!!!!!",
+    videoId: "https://naedist.animemusicquiz.com/8rksd7.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "A"
+      ]
+    },
+    sheetId: 20
+  },
+  {
+    name: "\"Asteroid Blues\" by Naomi Masuda",
+    videoId: "https://naedist.animemusicquiz.com/eyimbp.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "A"
+      ]
+    },
+    sheetId: 21
+  },
+  {
+    name: "\"Again\" by YUI",
+    videoId: "https://naedist.animemusicquiz.com/0rxibv.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "A"
+      ]
+    },
+    sheetId: 22
+  },
+  {
+    name: "\"Asterisk\" by Rie Kugimiya",
+    videoId: "https://naedist.animemusicquiz.com/0713jn.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "A"
+      ]
+    },
+    sheetId: 23
+  },
+  {
+    name: "\"A PERFECT HERO\" by CHRIS STANTON",
+    videoId: "https://naedist.animemusicquiz.com/m1ur8h.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "A"
+      ]
+    },
+    sheetId: 24
+  },
+  {
+    name: "\"ANGELUS\" by Hitomi Shimatani",
+    videoId: "https://naedist.animemusicquiz.com/gvjiy0.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "A"
+      ]
+    },
+    sheetId: 25
+  },
+  {
+    name: "\"Ao no Sumika\" by Tatsuya Kitani",
+    videoId: "https://naedist.animemusicquiz.com/3dzutngxr8betvz5.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "A"
+      ]
+    },
+    sheetId: 26
+  },
+  {
+    name: "\"ARIA\" by Kalafina",
+    videoId: "https://eudist.animemusicquiz.com/er8aki.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "A"
+      ]
+    },
+    sheetId: 27
+  },
+  {
+    name: "\"Akatsuki no Kuruma\" by FictionJunction YUUKA",
+    videoId: "https://naedist.animemusicquiz.com/2cw8ka.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "A"
+      ]
+    },
+    sheetId: 28
+  },
+  {
+    name: "\"ambiguous\" by GARNiDELiA",
+    videoId: "https://naedist.animemusicquiz.com/l8ct39.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "A"
+      ]
+    },
+    sheetId: 29
+  },
+  {
+    name: "\"Ambivalent\" by Uru",
+    videoId: "https://naedist.animemusicquiz.com/86jz91.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "A"
+      ]
+    },
+    sheetId: 30
+  },
+  {
+    name: "\"Alicemagic ~TV animation ver.~\" by Rita",
+    videoId: "https://eudist.animemusicquiz.com/rg6xey.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "A"
+      ]
+    },
+    sheetId: 31
+  },
+  {
+    name: "\"Awaken the power\" by Saint Aqours Snow",
+    videoId: "https://naedist.animemusicquiz.com/u5rkkt.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "A"
+      ]
+    },
+    sheetId: 32
+  },
+  {
+    name: "\"After, in the dark\" by Mai Yamane",
+    videoId: "https://naedist.animemusicquiz.com/w8plpb.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "A"
+      ]
+    },
+    sheetId: 33
+  },
+  {
+    name: "\"Ai no Scenario\" by CHICO with HoneyWorks",
+    videoId: "https://naedist.animemusicquiz.com/qinzxi.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "A"
+      ]
+    },
+    sheetId: 34
+  },
+  {
+    name: "\"Ame Nochi Niji-iro\" by Showtaro Morikubo\u00d7The Loose Dogs feat. Maki Ohguro",
+    videoId: "https://naedist.animemusicquiz.com/euyrpq.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "A"
+      ]
+    },
+    sheetId: 35
+  },
+  {
+    name: "\"Answer\" by Leo Ieiri",
+    videoId: "https://naedist.animemusicquiz.com/m90ypk.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "A"
+      ]
+    },
+    sheetId: 36
+  },
+  {
+    name: "\"Answer\" by BUMP OF CHICKEN",
+    videoId: "https://naedist.animemusicquiz.com/wcxc8n.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "A"
+      ]
+    },
+    sheetId: 37
+  },
+  {
+    name: "\"Authentic symphony\" by ChouCho",
+    videoId: "https://naedist.animemusicquiz.com/j9n4a6.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "A"
+      ]
+    },
+    sheetId: 38
+  },
+  {
+    name: "\"Ash Like Snow\" by the brilliant green",
+    videoId: "https://naedist.animemusicquiz.com/vhku1l.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "A"
+      ]
+    },
+    sheetId: 39
+  },
+  {
+    name: "\u201cAnna ni Issho Datta no ni\u201d by See-Saw",
+    videoId: "https://naedist.animemusicquiz.com/vy0wq8.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "A"
+      ]
+    },
+    sheetId: 40
+  },
+  {
+    name: "\"Aqua Terrarium\" by Nagi Yanagi",
+    videoId: "https://naedist.animemusicquiz.com/obd7ps.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "A"
+      ]
+    },
+    sheetId: 41
+  },
+  {
+    name: "\"Aimai na Kibou/Hisame\" by Emi Evans",
+    videoId: "https://naedist.animemusicquiz.com/g26jqz.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "A"
+      ]
+    },
+    sheetId: 42
+  },
+  {
+    name: "\"Ai no Melody\" by KOKIA",
+    videoId: "https://naedist.animemusicquiz.com/27p221.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "A"
+      ]
+    },
+    sheetId: 43
+  },
+  {
+    name: "\"Anata no Shiranai Koto\" by Rie Murakawa",
+    videoId: "https://naedist.animemusicquiz.com/s4jor5.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "A"
+      ]
+    },
+    sheetId: 44
+  },
+  {
+    name: "\"Above your hand\" by Annabel",
+    videoId: "https://naedist.animemusicquiz.com/i4gj7c.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "A"
+      ]
+    },
+    sheetId: 45
+  },
+  {
+    name: "\"Akuma no Ko\" by Ai Higuchi",
+    videoId: "https://naedist.animemusicquiz.com/fwrifz.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "A"
+      ]
+    },
+    sheetId: 46
+  },
+  {
+    name: "\"Anokanatarium\" by Mashumairesh!!",
+    videoId: "https://naedist.animemusicquiz.com/szzegb.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "A"
+      ]
+    },
+    sheetId: 47
+  },
+  {
+    name: "\"Aoi\" by Aimyon",
+    videoId: "https://naedist.animemusicquiz.com/mgvsba.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "A"
+      ]
+    },
+    sheetId: 48
+  },
+  {
+    name: "\"A\u2606I\u2606Tsu\" by dicot",
+    videoId: "https://naedist.animemusicquiz.com/pxf0ek.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "A"
+      ]
+    },
+    sheetId: 49
+  },
+  {
+    name: "\"Arc-en-ciel\" by HaNaTan",
+    videoId: "https://naedist.animemusicquiz.com/0w10e2.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "A"
+      ]
+    },
+    sheetId: 50
+  },
+  {
+    name: "\"Arcadia\" by Asami Imai",
+    videoId: "https://naedist.animemusicquiz.com/558091.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "A"
+      ]
+    },
+    sheetId: 51
+  },
+  {
+    name: "\"Aoi Tsuki to Ambivalence na Ai\" by moumoon",
+    videoId: "https://naedist.animemusicquiz.com/9d0tk7.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "A"
+      ]
+    },
+    sheetId: 52
+  },
+  {
+    name: "\"Agap\u0113\" by Melocure",
+    videoId: "https://naedist.animemusicquiz.com/wsgii1.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "A"
+      ]
+    },
+    sheetId: 53
+  },
+  {
+    name: "\"Ambitious World\" by Hina Tachibana & Hinaki Yano",
+    videoId: "https://naedist.animemusicquiz.com/lyocvl.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "A"
+      ]
+    },
+    sheetId: 54
+  },
+  {
+    name: "\u201cAnata no Soba ni.\" by Riria.",
+    videoId: "https://naedist.animemusicquiz.com/ar9ind.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "A"
+      ]
+    },
+    sheetId: 55
+  },
+  {
+    name: "\"Ari no Mama ni\" by Kana Sugiyama feat. Tokyo Shounen Shoujo Gasshou-tai",
+    videoId: "https://naedist.animemusicquiz.com/h7yfzt.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "A"
+      ]
+    },
+    sheetId: 56
+  },
+  {
+    name: "\"Ano Hi Time Machine\" by LONG SHOT PARTY",
+    videoId: "https://naedist.animemusicquiz.com/hf6932.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "A"
+      ]
+    },
+    sheetId: 57
+  }
+];
