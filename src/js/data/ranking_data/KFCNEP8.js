39e41cc47887cbeed6fba482ac89e6f5df692bc5
v <umar2le@gmail.com>
2026-06-05 20:52:09 -0700

    Added Nep 8

---
+++ src/js/data/ranking_data/KFCNEP8.js
@@ -0,0 +1,332 @@
+export const Nep8 = [
+  {
+    name: "Aenai",
+    videoId: "hjmL_4E0fdY",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "Nep8"
+      ]
+    },
+    sheetId: 1
+  },
+  {
+    name: "Asenai",
+    videoId: "QWTuoMR1ui0",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "Nep8"
+      ]
+    },
+    sheetId: 2
+  },
+  {
+    name: "blue hour",
+    videoId: "_w-cyLvGw28",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "Nep8"
+      ]
+    },
+    sheetId: 3
+  },
+  {
+    name: "Cat Loving",
+    videoId: "i4I9DXeOue0",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "Nep8"
+      ]
+    },
+    sheetId: 4
+  },
+  {
+    name: "Dance!",
+    videoId: "CzMaGbRO8Bs",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "Nep8"
+      ]
+    },
+    sheetId: 5
+  },
+  {
+    name: "days",
+    videoId: "W45JwAep6ro",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "Nep8"
+      ]
+    },
+    sheetId: 6
+  },
+  {
+    name: "Defy Gravity (Not Found 2010)",
+    videoId: "3tMO5zr3Zv4",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "Nep8"
+      ]
+    },
+    sheetId: 7
+  },
+  {
+    name: "Dramaturgy",
+    videoId: "hC22VEFRM4Q",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "Nep8"
+      ]
+    },
+    sheetId: 8
+  },
+  {
+    name: "Drawing a Wish",
+    videoId: "RVqdWj8YLFE",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "Nep8"
+      ]
+    },
+    sheetId: 9
+  },
+  {
+    name: "drop",
+    videoId: "I1wkvcoz6Mg",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "Nep8"
+      ]
+    },
+    sheetId: 10
+  },
+  {
+    name: "Junjou Kakumei",
+    videoId: "npS2-SX9wAM",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "Nep8"
+      ]
+    },
+    sheetId: 11
+  },
+  {
+    name: "Kagerou no Shirabe",
+    videoId: "CO15sYcowK8",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "Nep8"
+      ]
+    },
+    sheetId: 12
+  },
+  {
+    name: "Kimi no Kioku -Reload-",
+    videoId: "H5Qtu8fCycE",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "Nep8"
+      ]
+    },
+    sheetId: 13
+  },
+  {
+    name: "Kokoro Yohou",
+    videoId: "j98TxZs9Sh0",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "Nep8"
+      ]
+    },
+    sheetId: 14
+  },
+  {
+    name: "Little Dancer",
+    videoId: "uwA6a1cbDrk",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "Nep8"
+      ]
+    },
+    sheetId: 15
+  },
+  {
+    name: "Loud Hailer",
+    videoId: "gwGH2aP93Ho",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "Nep8"
+      ]
+    },
+    sheetId: 16
+  },
+  {
+    name: "Love Slave",
+    videoId: "D8EAoygYk7w",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "Nep8"
+      ]
+    },
+    sheetId: 17
+  },
+  {
+    name: "madder",
+    videoId: "tL4TkRd7zkE",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "Nep8"
+      ]
+    },
+    sheetId: 18
+  },
+  {
+    name: "Murasakiiro no Himawari",
+    videoId: "B5X3VlWeUWY",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "Nep8"
+      ]
+    },
+    sheetId: 19
+  },
+  {
+    name: "my crush",
+    videoId: "KxmplNnFPvg",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "Nep8"
+      ]
+    },
+    sheetId: 20
+  },
+  {
+    name: "Nemurenai",
+    videoId: "FM9AySPC9Ak",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "Nep8"
+      ]
+    },
+    sheetId: 21
+  },
+  {
+    name: "Precious times",
+    videoId: "2PMuotJEaNo",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "Nep8"
+      ]
+    },
+    sheetId: 22
+  },
+  {
+    name: "Saibou Prominence",
+    videoId: "2S65OckmJoo",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "Nep8"
+      ]
+    },
+    sheetId: 23
+  },
+  {
+    name: "Sakura no Ame Moegi no Yo",
+    videoId: "YWVJ122bB7Y",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "Nep8"
+      ]
+    },
+    sheetId: 24
+  },
+  {
+    name: "Senkou",
+    videoId: "I0s49imrtho",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "Nep8"
+      ]
+    },
+    sheetId: 25
+  },
+  {
+    name: "Sorairo Days",
+    videoId: "PKRUKalbx3s",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "Nep8"
+      ]
+    },
+    sheetId: 26
+  },
+  {
+    name: "Take Over",
+    videoId: "lk8BMgl9vp0",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "Nep8"
+      ]
+    },
+    sheetId: 27
+  },
+  {
+    name: "Tokohana",
+    videoId: "tEv9HsTyPmM",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "Nep8"
+      ]
+    },
+    sheetId: 28
+  },
+  {
+    name: "Umitsuki",
+    videoId: "q9YrWVr4dvI",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "Nep8"
+      ]
+    },
+    sheetId: 29
+  },
+  {
+    name: "Yoka ni Mitorete",
+    videoId: "CaWK1USG5BA",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "Nep8"
+      ]
+    },
+    sheetId: 30
+  }
+];
