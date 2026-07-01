512cc1ff1183e4f69c708196e57018d0e83afa38
v <umar2le@gmail.com>
2026-06-30 18:56:55 -0700

    Added B

---
+++ src/js/data/ranking_data/B.js
@@ -0,0 +1,750 @@
+export const B = [
+  {
+    name: "\"Believe\u00d7Believe\" by Nozomi Yamamoto",
+    videoId: "https://naedist.animemusicquiz.com/e62x1b.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 1
+  },
+  {
+    name: "\"Believe it\" by Karen\u30fbMirai from BEST FRIENDS!",
+    videoId: "https://naedist.animemusicquiz.com/kajfdn.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 2
+  },
+  {
+    name: "\"BRE@TH//LESS\" by Mika Kobayashi",
+    videoId: "https://naedist.animemusicquiz.com/ssxe97.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 3
+  },
+  {
+    name: "\"Blue Destiny\" by Trident",
+    videoId: "https://naedist.animemusicquiz.com/rif5aw.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 4
+  },
+  {
+    name: "\"Bull's Eye\" by nano",
+    videoId: "https://naedist.animemusicquiz.com/zj2iq8.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 5
+  },
+  {
+    name: "\"Baukl\u00f6tze\" by Mika Kobayashi",
+    videoId: "https://naedist.animemusicquiz.com/u21mre.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 6
+  },
+  {
+    name: "\"Beautiful Birthday\" by RAISE A SUILEN",
+    videoId: "https://naedist.animemusicquiz.com/1x4hfm.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 7
+  },
+  {
+    name: "\"BLOODY POWER FAME\" by coldrain",
+    videoId: "https://naedist.animemusicquiz.com/77ly8n.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 8
+  },
+  {
+    name: "\"Black Catcher\" by Vickeblanka",
+    videoId: "https://eudist.animemusicquiz.com/owg02q.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 9
+  },
+  {
+    name: "\"BACK ON MY FEET\" by BOOM BOOM SATELLITES",
+    videoId: "https://naedist.animemusicquiz.com/iku914.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 10
+  },
+  {
+    name: "\"Believers High\" by flumpool",
+    videoId: "https://naedist.animemusicquiz.com/sqdlhx.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 11
+  },
+  {
+    name: "\"BLACK LOTUS\" by RONDO from D4DJ",
+    videoId: "https://naedist.animemusicquiz.com/1uzquk.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 12
+  },
+  {
+    name: "\"Beautiful World\" by Hikaru Utada",
+    videoId: "https://naedist.animemusicquiz.com/6e81wt.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 13
+  },
+  {
+    name: "\"Be Survivor\" by ZZ",
+    videoId: "https://naedist.animemusicquiz.com/69wxh1.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 14
+  },
+  {
+    name: "\"Brave Shine\" by Aimer",
+    videoId: "https://naedist.animemusicquiz.com/hfybji.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 15
+  },
+  {
+    name: "\"believe\" by Kalafina",
+    videoId: "https://naedist.animemusicquiz.com/j8l5ox.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 16
+  },
+  {
+    name: "\"Binary Star\" by SawanoHiroyuki[nZk]:Uru",
+    videoId: "https://naedist.animemusicquiz.com/o2pwjj.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 17
+  },
+  {
+    name: "\"balance unbalance ~Hontou no Watashi~\" by Suzuko Mimori",
+    videoId: "https://naedist.animemusicquiz.com/0pgsv1.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 18
+  },
+  {
+    name: "\"Break\" by Uru",
+    videoId: "https://naedist.animemusicquiz.com/qbcc9u.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 19
+  },
+  {
+    name: "\"Be the light\" by ONE OK ROCK",
+    videoId: "https://naedist.animemusicquiz.com/52z0e9.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 20
+  },
+  {
+    name: "\"behind\" by Karin Isobe, Yuna Yoshino, Lynn",
+    videoId: "https://naedist.animemusicquiz.com/mi9q1b.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 21
+  },
+  {
+    name: "\"Bleed My Heart\" by Alisa",
+    videoId: "https://naedist.animemusicquiz.com/w4vdk6.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 22
+  },
+  {
+    name: "\"BROKEN MIRROR\" by BOOM BOOM SATELLITES",
+    videoId: "https://naedist.animemusicquiz.com/wqg0kd.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 23
+  },
+  {
+    name: "\"bL\u221edy f8 -eUC-\" by \"SawanoHiroyuki[nZk]:Aimer\"",
+    videoId: "https://naedist.animemusicquiz.com/h8hdtc.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 24
+  },
+  {
+    name: "\"Be My Angel\" by Atsuko Enomoto",
+    videoId: "https://naedist.animemusicquiz.com/k48dc7.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 25
+  },
+  {
+    name: "\"Bye Bye\" by 7!!",
+    videoId: "https://naedist.animemusicquiz.com/rhqp0m.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 26
+  },
+  {
+    name: "\"Balalaika\" by Koharu Kusumi",
+    videoId: "https://naedist.animemusicquiz.com/uy2wlo.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 27
+  },
+  {
+    name: "\"Bokura wa Family\" by KERA",
+    videoId: "https://naedist.animemusicquiz.com/njitic.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 28
+  },
+  {
+    name: "\"Buddy\" by Maaya Sakamoto",
+    videoId: "https://naedist.animemusicquiz.com/e0ag23.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 29
+  },
+  {
+    name: "\"Boys be Smile\" by Suzuyu",
+    videoId: "https://naedist.animemusicquiz.com/hahu4q.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 30
+  },
+  {
+    name: "\"Boku-tachi wa Hitotsu no Hikari\" by \u03bc's",
+    videoId: "https://naedist.animemusicquiz.com/6ed4ik.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 31
+  },
+  {
+    name: "\"Bokura no Senjou\" by Walk\u00fcre",
+    videoId: "https://naedist.animemusicquiz.com/8g23va.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 32
+  },
+  {
+    name: "\"Beyond\" by Mika Nakashima",
+    videoId: "https://naedist.animemusicquiz.com/xumkv6n97d126wnb.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 33
+  },
+  {
+    name: "\"Born to be\" by nano",
+    videoId: "https://naedist.animemusicquiz.com/oi3t2y.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 34
+  },
+  {
+    name: "\"BRIGHT STREAM\" byNana Mizuki",
+    videoId: "https://nawdist.animemusicquiz.com/urko9j.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 35
+  },
+  {
+    name: "\"BOW AND ARROW\" by Kenshi Yonezu",
+    videoId: "https://naedist.animemusicquiz.com/n0yfu8.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 36
+  },
+  {
+    name: "\"Bara-iro no Jinsei\" by Mai Kuraki",
+    videoId: "https://naedist.animemusicquiz.com/ii1j9r.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 37
+  },
+  {
+    name: "\"Black\u2020White\" by Iori Nomizu",
+    videoId: "https://naedist.animemusicquiz.com/gayg6m.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 38
+  },
+  {
+    name: "\"Black Holy\" by Mikako Komatsu",
+    videoId: "https://naedist.animemusicquiz.com/mw2xbm.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 39
+  },
+  {
+    name: "\"Beyond fate\" by Mion Yano",
+    videoId: "https://naedist.animemusicquiz.com/jiu3b1.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 40
+  },
+  {
+    name: "\"Beautiful\" by Anly",
+    videoId: "https://naedist.animemusicquiz.com/0l9v3h.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 41
+  },
+  {
+    name: "\"Broken Youth\" by NICO Touches the Walls",
+    videoId: "https://naedist.animemusicquiz.com/1rvcr0.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 42
+  },
+  {
+    name: "\"Blue Bird\" by Ikimono-gakari",
+    videoId: "https://naedist.animemusicquiz.com/enrlw0.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 43
+  },
+  {
+    name: "\"Boku ni Dekiru Koto\" by HOW MERRY MARRY",
+    videoId: "https://naedist.animemusicquiz.com/ojfzqz.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 44
+  },
+  {
+    name: "\"BON VOYAGE!\" by BON-BON BLANCO",
+    videoId: "https://naedist.animemusicquiz.com/m2wa5w.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 45
+  },
+  {
+    name: "\"BEFORE DAWN\" by AI-SACHI",
+    videoId: "https://naedist.animemusicquiz.com/hafzpk.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 46
+  },
+  {
+    name: "\"Burning\" by Hitsujibungaku",
+    videoId: "https://naedist.animemusicquiz.com/ipy1d4.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 47
+  },
+  {
+    name: "\"Burn My Dread -Spring of Birth Ver.-\" by Yumi Kawamura",
+    videoId: "https://naedist.animemusicquiz.com/nig358.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 48
+  },
+  {
+    name: "\"Boku no Sonzai Shoumei\" by Etsuko Yakushimaru Metro Orchestra",
+    videoId: "https://naedist.animemusicquiz.com/xb316t.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 49
+  },
+  {
+    name: "\"Believe in you\" by nonoc",
+    videoId: "https://naedist.animemusicquiz.com/iw13fy.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 50
+  },
+  {
+    name: "\"Button\" by PENGUIN RESEARCH",
+    videoId: "https://naedist.animemusicquiz.com/ylphx6.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 51
+  },
+  {
+    name: "\"Black Swallowtail\" by UROBOROS",
+    videoId: "https://naedist.animemusicquiz.com/8u03oq.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 52
+  },
+  {
+    name: "\"bloooomin'\" by Little Non",
+    videoId: "https://naedist.animemusicquiz.com/ky6wwc.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 53
+  },
+  {
+    name: "\"being\" by KOTOKO",
+    videoId: "https://naedist.animemusicquiz.com/hdhrg5.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 54
+  },
+  {
+    name: "\"BLUE\" by Ritsuko Tanaka",
+    videoId: "https://naedist.animemusicquiz.com/hj0x0h.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 55
+  },
+  {
+    name: "\"BLACK DIAMOND (Major Version)\" by Black Diamonds",
+    videoId: "https://naedist.animemusicquiz.com/vpk8xv.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 56
+  },
+  {
+    name: "\"bliss\" by milet",
+    videoId: "https://naedist.animemusicquiz.com/as6rwg.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 57
+  },
+  {
+    name: "\"BEYOND\" by Miho Fukuhara",
+    videoId: "https://naedist.animemusicquiz.com/llwahq.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 58
+  },
+  {
+    name: "\"Beautiful Life\" by Sotte Bosse",
+    videoId: "https://naedist.animemusicquiz.com/6v0p71.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 59
+  },
+  {
+    name: "\"Blue sky, True sky\" by Aira Yuuki",
+    videoId: "https://naedist.animemusicquiz.com/3kz0du.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 60
+  },
+  {
+    name: "\"Boku wa...\" by Atarayo",
+    videoId: "https://naedist.animemusicquiz.com/nzt5qc.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 61
+  },
+  {
+    name: "\"BLIZZARD\" by BURNOUT SYNDROMES",
+    videoId: "https://naedist.animemusicquiz.com/lcluit.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 62
+  },
+  {
+    name: "\"BLAZE\" by Kinya",
+    videoId: "https://naedist.animemusicquiz.com/qrsxtz.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 63
+  },
+  {
+    name: "\"Best of my Love\" by Rei Yasuda",
+    videoId: "https://naedist.animemusicquiz.com/o3l0ym.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 64
+  },
+  {
+    name: "\"BRIGHTEST HEART\" by Tomoyo Takayanagi",
+    videoId: "https://eudist.animemusicquiz.com/73a74v.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 65
+  },
+  {
+    name: "\"blan_\" by Arika",
+    videoId: "https://naedist.animemusicquiz.com/3wu8xf.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 66
+  },
+  {
+    name: "\"BELIEVE\u2606MY VOICE\" by Mamoru Miyano",
+    videoId: "https://naedist.animemusicquiz.com/h3y7zi.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 67
+  },
+  {
+    name: "\"Breakin' Down\" by Laco",
+    videoId: "https://files.catbox.moe/5wa08c.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "B"
+      ]
+    },
+    sheetId: 68
+  }
+];
