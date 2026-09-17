4a93f31bb47b92c8f2159e91375bf368168d9aa4
v <umar2le@gmail.com>
2026-09-16 19:06:50 -0700

    Added Nep 9

---
+++ src/js/data/ranking_data/Nep9.js
@@ -0,0 +1,244 @@
+export const Nep9 = [
+  {
+    name: "Ao to Natsu",
+    videoId: "m34DPnRUfMU",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "Nep9"
+      ]
+    },
+    sheetId: 1
+  },
+  {
+    name: "Badfish",
+    videoId: "rmadSGJCzo8",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "Nep9"
+      ]
+    },
+    sheetId: 2
+  },
+  {
+    name: "Floria",
+    videoId: "l3oDiGuuALk",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "Nep9"
+      ]
+    },
+    sheetId: 3
+  },
+  {
+    name: "Hachigatsu, Nanigashi, Tsukiakari",
+    videoId: "Vs5NViM8TSY",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "Nep9"
+      ]
+    },
+    sheetId: 4
+  },
+  {
+    name: "Henceforth",
+    videoId: "apj5_-osMd4",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "Nep9"
+      ]
+    },
+    sheetId: 5
+  },
+  {
+    name: "Island in the Sun",
+    videoId: "erG5rgNYSdk",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "Nep9"
+      ]
+    },
+    sheetId: 6
+  },
+  {
+    name: "Kimi to Semi Blue",
+    videoId: "Z-LWjF5J6Mw",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "Nep9"
+      ]
+    },
+    sheetId: 7
+  },
+  {
+    name: "Message of a Rainbow",
+    videoId: "M2pCij2qlpM",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "Nep9"
+      ]
+    },
+    sheetId: 8
+  },
+  {
+    name: "Natsu ga Bokura wo Yobu no Nara",
+    videoId: "9imeJEtOG5A",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "Nep9"
+      ]
+    },
+    sheetId: 9
+  },
+  {
+    name: "Natsuiro Egao de 1,2,Jump!",
+    videoId: "1j-u11dHWnI",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "Nep9"
+      ]
+    },
+    sheetId: 10
+  },
+  {
+    name: "Natsutourou",
+    videoId: "2epJQyYSF2Q",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "Nep9"
+      ]
+    },
+    sheetId: 11
+  },
+  {
+    name: "ONE",
+    videoId: "PMsCv0ceySw",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "Nep9"
+      ]
+    },
+    sheetId: 12
+  },
+  {
+    name: "Otsukare SUMMER",
+    videoId: "ViFHruS6oO8",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "Nep9"
+      ]
+    },
+    sheetId: 13
+  },
+  {
+    name: "PARTY",
+    videoId: "HQzu7NYlZNQ",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "Nep9"
+      ]
+    },
+    sheetId: 14
+  },
+  {
+    name: "Ramune",
+    videoId: "U8rvZpWr4Wg",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "Nep9"
+      ]
+    },
+    sheetId: 15
+  },
+  {
+    name: "Routine",
+    videoId: "MYbRVT4M4ho",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "Nep9"
+      ]
+    },
+    sheetId: 16
+  },
+  {
+    name: "Summer",
+    videoId: "MYZXUJ-BG-Q",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "Nep9"
+      ]
+    },
+    sheetId: 17
+  },
+  {
+    name: "Summertime Friends",
+    videoId: "tZdMX7ohw1I",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "Nep9"
+      ]
+    },
+    sheetId: 18
+  },
+  {
+    name: "Tonde Icchaitai no",
+    videoId: "_sxe7PIxKQk",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "Nep9"
+      ]
+    },
+    sheetId: 19
+  },
+  {
+    name: "Ukigumo",
+    videoId: "EFdAzKntuAk",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "Nep9"
+      ]
+    },
+    sheetId: 20
+  },
+  {
+    name: "White Promise",
+    videoId: "CzDFI5obORE",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "Nep9"
+      ]
+    },
+    sheetId: 21
+  },
+  {
+    name: "Yummy Summer",
+    videoId: "iczN_AnpWYA",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "Nep9"
+      ]
+    },
+    sheetId: 22
+  }
+];
