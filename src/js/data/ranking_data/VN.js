11072dc35d55f8eb2d77c5c4153cb6ba5ae97adb
v <umar2le@gmail.com>
2026-09-17 22:01:40 -0700

    added VN

---
+++ src/js/data/ranking_data/VN.js
@@ -0,0 +1,398 @@
+export const VN = [
+  {
+    name: "Blue Skies",
+    videoId: "https://amqbot.082640.xyz/files/adbfad22e1fdc89dea02be3e5e1ae955.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "VN"
+      ]
+    },
+    sheetId: 1
+  },
+  {
+    name: "Re: Nandome ka no Hajimemashite",
+    videoId: "https://amqbot.082640.xyz/files/6d1ccad4735ac1dc7b0c9e4297f69af7.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "VN"
+      ]
+    },
+    sheetId: 2
+  },
+  {
+    name: "Eternicle",
+    videoId: "7Od2hsQC7gg",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "VN"
+      ]
+    },
+    sheetId: 3
+  },
+  {
+    name: "sign",
+    videoId: "c8rjvvhiTcw",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "VN"
+      ]
+    },
+    sheetId: 4
+  },
+  {
+    name: "Dreams come true -Hanasaki hokoru, Hoshi no Moto de-",
+    videoId: "q0hkOrHr0Z8",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "VN"
+      ]
+    },
+    sheetId: 5
+  },
+  {
+    name: "Judge of a Life",
+    videoId: "g7haN5U3J1c",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "VN"
+      ]
+    },
+    sheetId: 6
+  },
+  {
+    name: "Ima, Aruki dasu Kimi e",
+    videoId: "ZQqbhwH87OM",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "VN"
+      ]
+    },
+    sheetId: 7
+  },
+  {
+    name: "HAPPY for GOOD",
+    videoId: "https://amqbot.082640.xyz/files/a781fb6e7ff552b83ed2172f14fade64.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "VN"
+      ]
+    },
+    sheetId: 8
+  },
+  {
+    name: "Hikari",
+    videoId: "https://files.catbox.moe/sli30x.flac",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "VN"
+      ]
+    },
+    sheetId: 9
+  },
+  {
+    name: "Convallaria ",
+    videoId: "https://files.catbox.moe/u6mwgz.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "VN"
+      ]
+    },
+    sheetId: 10
+  },
+  {
+    name: "Tomadoi",
+    videoId: "clGvQ1G-ESo",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "VN"
+      ]
+    },
+    sheetId: 11
+  },
+  {
+    name: "You Need Me",
+    videoId: "https://files.catbox.moe/7y8cc3.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "VN"
+      ]
+    },
+    sheetId: 12
+  },
+  {
+    name: "Find The Gate",
+    videoId: "ZB1gsAr5njQ",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "VN"
+      ]
+    },
+    sheetId: 13
+  },
+  {
+    name: "Negai",
+    videoId: "Cu4ZaZj6wNw",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "VN"
+      ]
+    },
+    sheetId: 14
+  },
+  {
+    name: "remember you forever",
+    videoId: "dqCINq_VydI",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "VN"
+      ]
+    },
+    sheetId: 15
+  },
+  {
+    name: "Hajimari no Toki",
+    videoId: "https://files.catbox.moe/kitom6.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "VN"
+      ]
+    },
+    sheetId: 16
+  },
+  {
+    name: "Kimi o Nosete",
+    videoId: "https://files.catbox.moe/8x8377.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "VN"
+      ]
+    },
+    sheetId: 17
+  },
+  {
+    name: "My Spring",
+    videoId: "jhvIgc9gtBQ",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "VN"
+      ]
+    },
+    sheetId: 18
+  },
+  {
+    name: "Tick-tack",
+    videoId: "MkqapY24TkU",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "VN"
+      ]
+    },
+    sheetId: 19
+  },
+  {
+    name: "Hitohira",
+    videoId: "https://file.garden/anMFQhKssQGmKFMI/hitohira.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "VN"
+      ]
+    },
+    sheetId: 20
+  },
+  {
+    name: "Ichi-oku Bun no Ichi Centimeter",
+    videoId: "https://file.garden/anMFQhKssQGmKFMI/%E3%80%90%E5%AD%90%E5%AE%89%E6%AD%A6%E4%BA%BA%E3%80%91%E4%B8%80%E5%84%84%E5%88%86%E3%81%AE%EF%BC%91%E3%82%BB%E3%83%B3%E3%83%81.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "VN"
+      ]
+    },
+    sheetId: 21
+  },
+  {
+    name: "Taichourou",
+    videoId: "https://file.garden/alwN97Y5rARmvcVO/7e74bc77-d2ab-4dec-97e7-2a0318965268.weba",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "VN"
+      ]
+    },
+    sheetId: 22
+  },
+  {
+    name: "Kanousei no Memoria",
+    videoId: "https://files.catbox.moe/benr3j.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "VN"
+      ]
+    },
+    sheetId: 23
+  },
+  {
+    name: "Tsuisou Canon",
+    videoId: "nfl3g4VHKNk",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "VN"
+      ]
+    },
+    sheetId: 24
+  },
+  {
+    name: "Unveil",
+    videoId: "ORQff1P24cY",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "VN"
+      ]
+    },
+    sheetId: 25
+  },
+  {
+    name: "Zan'ei no Yoru ga Akeru Toki",
+    videoId: "https://files.catbox.moe/nma4im.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "VN"
+      ]
+    },
+    sheetId: 26
+  },
+  {
+    name: "Princess Blood",
+    videoId: "TlmHrFuW--8",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "VN"
+      ]
+    },
+    sheetId: 27
+  },
+  {
+    name: "Koko ni Iru yo",
+    videoId: "https://amqbot.082640.xyz/files/79603570061d1cac6bef0bdf786962f0.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "VN"
+      ]
+    },
+    sheetId: 28
+  },
+  {
+    name: "a flavor of the moment",
+    videoId: "Q2mJJnC6lOc",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "VN"
+      ]
+    },
+    sheetId: 29
+  },
+  {
+    name: "Riot crimson",
+    videoId: "https://file.garden/almOuhnllGIweQbM/riotcrimsonnew.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "VN"
+      ]
+    },
+    sheetId: 30
+  },
+  {
+    name: "En Route",
+    videoId: "https://file.garden/alwN97Y5rARmvcVO/en%20route.mp3?v=1786217009002",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "VN"
+      ]
+    },
+    sheetId: 31
+  },
+  {
+    name: "Ritual",
+    videoId: "4xMZ3Ph037E",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      ranking: [
+        "VN"
+      ]
+    },
+    sheetId: 32
+  },
+  {
+    name: "Crescent Maze",
+    videoId: "https://file.garden/alwN97Y5rARmvcVO/Crescent%20Maze.mp3?v=1786216677413",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "VN"
+      ]
+    },
+    sheetId: 33
+  },
+  {
+    name: "Sagashimono",
+    videoId: "https://amqbot-files.082640.xyz/files/20d41774eaf999fb432c8cb1d8e708e1.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "VN"
+      ]
+    },
+    sheetId: 34
+  },
+  {
+    name: "Ten'yoku no Asklein",
+    videoId: "https://files.catbox.moe/auey7s.mp3",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "VN"
+      ]
+    },
+    sheetId: 35
+  },
+  {
+    name: "Yume no Tabiji",
+    videoId: "https://amqbot-files.082640.xyz/files/3fe46c5a6f71d2d5214c5b282d00ae4c.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      ranking: [
+        "VN"
+      ]
+    },
+    sheetId: 36
+  }
+];
